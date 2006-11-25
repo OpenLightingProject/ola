4992d156ad2277fa12a1a750390bfd4611a57f69
simon <simon@windu.localdomain>
2006-11-25 09:49:49 -0800

    * test

--- lla/lla.c
+++ lla/lla.c
@@ -13,7 +13,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * lla.c
+ * $file$
  * Main file for liblla
  * Copyright (C) 2005  Simon Newton
  */
