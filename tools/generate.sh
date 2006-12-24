4c0723f7c7d8f1b3868c1ceb9fdbfe734d53dee2
simon <simon@windu.localdomain>
2006-12-23 20:33:37 -0800

    * fixed

--- tools/generate.sh
+++ tools/generate.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-./generate.pl ../plugins/usbpro/UsbProConfMessages.xml
-cp lla-UsbPro/* ../plugins/usbpro/conf-msgs/
+XML_SOURCES="../plugins/usbpro/UsbProConfMessages.xml"
+
+for src in $XML_SOURCES; do
+	if [ ! -f $src ]; then
+		echo Could not find $src;
+		exit 1;
+	fi
+
+	./generate.pl $src
+
+	# fix me
+	cp lla-UsbPro/* ../plugins/usbpro/conf_msgs/
+done
