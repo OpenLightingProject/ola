674245c6251b53eb3d48f61cf972b87e2c9a8c50
simon <simon@windu.localdomain>
2007-10-14 15:26:35 -0700

    * fixed typo in pathport plugin doc

--- plugins/stageprofi/StageProfiPlugin.cpp
+++ plugins/stageprofi/StageProfiPlugin.cpp
@@ -131,7 +131,7 @@ string StageProfiPlugin::get_desc() const {
 "\n"
 "device = /dev/ttyUSB0\n"
 "device = 192.168.1.250\n"
-"The device to use either as a part for the USB version or an IP address\n"
+"The device to use either as a path for the USB version or an IP address\n"
 "for the LAN version. Multiple devices are supported.\n";
 }
 
