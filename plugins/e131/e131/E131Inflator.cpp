da09f6725ab9f3f8b62c51b6ab406b20a2c96c75
simon <simon@localhost.localdomain>
2010-01-15 23:56:02 -0800

    * fix a bug in calculating the option in the E1.31 header

--- plugins/e131/e131/E131Inflator.cpp
+++ plugins/e131/e131/E131Inflator.cpp
@@ -56,8 +56,8 @@ bool E131Inflator::DecodeHeader(HeaderSet &headers,
           raw_header->priority,
           raw_header->sequence,
           NetworkToHost(raw_header->universe),
-          raw_header->options && E131Header::PREVIEW_DATA_MASK,
-          raw_header->options && E131Header::STREAM_TERMINATED_MASK);
+          raw_header->options & E131Header::PREVIEW_DATA_MASK,
+          raw_header->options & E131Header::STREAM_TERMINATED_MASK);
       m_last_header = header;
       m_last_header_valid = true;
       headers.SetE131Header(header);
