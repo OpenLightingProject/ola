59114f26cd31487d4a50420d3e5259047f4c69ea
Simon Newton <simonn@simonn-macbookpro.local>
2010-04-18 12:59:18 -0700

    * first pass at the anyma plugin

---
+++ plugins/usbdmx/AnymaDevice.cpp
@@ -0,0 +1,50 @@
+/*
+ *  This dmxgram is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This dmxgram is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this dmxgram; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * AnymaDevice.cpp
+ * The Anyma usb driver
+ * Copyright (C) 2010 Simon Newton
+ */
+
+#include <string.h>
+#include <sys/time.h>
+#include <string>
+
+#include "ola/Logging.h"
+#include "plugins/usbdmx/AnymaDevice.h"
+#include "plugins/usbdmx/AnymaOutputPort.h"
+
+namespace ola {
+namespace plugin {
+namespace usbdmx {
+
+
+/*
+ * Start this device.
+ */
+bool AnymaDevice::StartHook() {
+  AnymaOutputPort *output_port = new AnymaOutputPort(this,
+                                                     0,
+                                                     m_usb_device);
+  if (!output_port->Start()) {
+    delete output_port;
+    return false;
+  }
+  AddPort(output_port);
+  return true;
+}
+}  // usbdmx
+}  // plugin
+}  // ola
---
+++ plugins/usbdmx/AnymaDevice.h
@@ -0,0 +1,50 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This dmxgram is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this dmxgram; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * AnymaDevice.h
+ * Interface for the Anyma device
+ * Copyright (C) 2010 Simon Newton
+ */
+
+#ifndef PLUGINS_USBDMX_ANYMADEVICE_H_
+#define PLUGINS_USBDMX_ANYMADEVICE_H_
+
+#include <libusb.h>
+#include <string>
+#include "plugins/usbdmx/UsbDevice.h"
+
+namespace ola {
+namespace plugin {
+namespace usbdmx {
+
+/*
+ * A Anyma device
+ */
+class AnymaDevice: public UsbDevice {
+  public:
+    AnymaDevice(ola::AbstractPlugin *owner,
+                libusb_device *usb_device):
+        UsbDevice(owner, "Anyma USB Device", usb_device) {
+    }
+
+    string DeviceId() const { return "anyma"; }
+
+  protected:
+    bool StartHook();
+};
+}  // usbdmx
+}  // plugin
+}  // ola
+#endif  // PLUGINS_USBDMX_ANYMADEVICE_H_
