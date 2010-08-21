91ed96d438599b6a5d83029319a77b8925d9e4fe
Simon Newton <simonn@simonn-macbookpro.local>
2010-08-21 13:14:52 -0700

    * add copyright

--- logger.js
+++ logger.js
@@ -1,3 +1,22 @@
+/**
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * The class that handles the logging.
+ * Copyright (C) 2010 Simon Newton
+ */
+
 goog.require('goog.debug.DivConsole');
 goog.require('goog.debug.LogManager');
 goog.require('goog.debug.Logger');
