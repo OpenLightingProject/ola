c70a07a59fa9ddc8e9c35ede5f6c1ba6bf435c80
Simon Newton <simonn@simonn-macbookpro.local>
2010-09-05 22:48:22 -0700

    * fix another bug

--- common/rdm/RDMCommand.cpp
+++ common/rdm/RDMCommand.cpp
@@ -412,7 +412,7 @@ RDMResponse *NackWithReason(const RDMRequest *request,
                             rdm_nack_reason reason_enum) {
   uint16_t reason = ola::network::HostToNetwork(static_cast<uint16_t>(
     reason_enum));
-  if (request->CommandClass() == ola::rdm::RDMCommand::GET_COMMAND) {
+  if (request->CommandClass() == ola::rdm::RDMCommand::GET_COMMAND_RESPONSE) {
     return new ola::rdm::RDMGetResponse(
       request->DestinationUID(),
       request->SourceUID(),
