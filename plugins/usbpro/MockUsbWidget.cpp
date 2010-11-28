3e734f232d7f3a34667d3bb9f3eaf6f54562d4e5
Simon Newton <simonn@simonn-macbookpro.local>
2010-11-27 23:31:33 -0800

    * move the MockUsbWidget into it's own file so it can be reused

---
+++ plugins/usbpro/MockUsbWidget.cpp
@@ -0,0 +1,119 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * MockUsbWidget.cpp
+ * The mock usb widget used for testing
+ * Copyright (C) 2010 Simon Newton
+ */
+
+#include <string.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <queue>
+
+#include "ola/Callback.h"
+#include "ola/Logging.h"
+#include "plugins/usbpro/UsbWidget.h"
+
+
+/**
+ * The MockUsbWidget, used to verify calls.
+ */
+class MockUsbWidget: public ola::plugin::usbpro::UsbWidgetInterface {
+  public:
+    MockUsbWidget():
+      m_callback(NULL) {}
+    ~MockUsbWidget() {
+      if (m_callback)
+        delete m_callback;
+    }
+
+    void SetMessageHandler(
+        ola::Callback3<void, uint8_t, const uint8_t*,
+                       unsigned int> *callback) {
+      if (m_callback)
+        delete m_callback;
+      m_callback = callback;
+    }
+
+    // this doesn't do anything
+    void SetOnRemove(ola::SingleUseCallback0<void> *on_close) {
+      delete on_close;
+    }
+
+    bool SendMessage(uint8_t label,
+                     const uint8_t *data,
+                     unsigned int length) const;
+
+    void AddExpectedCall(uint8_t expected_label,
+                         const uint8_t *expected_data,
+                         unsigned int expected_length,
+                         uint8_t return_label,
+                         const uint8_t *return_data,
+                         unsigned int return_length);
+
+    void Verify() {
+      CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(0), m_expected_calls.size());
+    }
+
+  private:
+    ola::Callback3<void, uint8_t, const uint8_t*, unsigned int> *m_callback;
+
+    typedef struct {
+      uint8_t label;
+      unsigned int length;
+      const uint8_t *data;
+    } command;
+
+    typedef struct {
+      command expected_command;
+      command return_command;
+    } expected_call;
+
+    mutable std::queue<expected_call> m_expected_calls;
+};
+
+
+bool MockUsbWidget::SendMessage(uint8_t label,
+                                const uint8_t *data,
+                                unsigned int length) const {
+  CPPUNIT_ASSERT(m_expected_calls.size());
+  expected_call call = m_expected_calls.front();
+  m_expected_calls.pop();
+  CPPUNIT_ASSERT_EQUAL(call.expected_command.label, label);
+  CPPUNIT_ASSERT_EQUAL(call.expected_command.length, length);
+  CPPUNIT_ASSERT(!memcmp(call.expected_command.data, data, length));
+
+  m_callback->Run(call.return_command.label,
+                  call.return_command.data,
+                  call.return_command.length);
+  return true;
+}
+
+
+void MockUsbWidget::AddExpectedCall(uint8_t expected_label,
+                                    const uint8_t *expected_data,
+                                    unsigned int expected_length,
+                                    uint8_t return_label,
+                                    const uint8_t *return_data,
+                                    unsigned int return_length) {
+  expected_call call;
+  call.expected_command.label = expected_label;
+  call.expected_command.data = expected_data;
+  call.expected_command.length = expected_length;
+  call.return_command.label = return_label;
+  call.return_command.data = return_data;
+  call.return_command.length = return_length;
+  m_expected_calls.push(call);
+}
