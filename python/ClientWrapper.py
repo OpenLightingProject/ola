a42b75bd7eec4b823dd37d019f286a4474f411d7
Simon Newton <simonn@simonn-macbookpro-2.local>
2011-01-19 21:40:08 -0800

    * add the ability to schedule callbacks in the client wrapper

--- python/ClientWrapper.py
+++ python/ClientWrapper.py
@@ -20,9 +20,36 @@
 
 __author__ = 'nomis52@gmail.com (Simon Newton)'
 
+import datetime
+import heapq
 import socket
 import select
 from ola.OlaClient import OlaClient, Universe
+
+
+class Event(object):
+  def __init__(self, time_ms, callback):
+    self._run_at = (datetime.datetime.now() +
+                    datetime.timedelta(milliseconds = time_ms))
+    print self._run_at
+    self._callback = callback
+
+  def __cmp__(self, other):
+    return cmp(self._run_at, other._run_at)
+
+  def TimeLeft(self, now):
+    """Returns the number of seconds until this event."""
+    time_delta = self._run_at - now
+    seconds =  time_delta.seconds + time_delta.days * 24 * 3600
+    seconds += time_delta.microseconds / 10.0**6
+    print "time left %f" % seconds
+    return seconds
+
+  def HasExpired(self, now):
+    return self._run_at < now
+
+  def Run(self):
+    self._callback()
 
 
 class ClientWrapper(object):
@@ -31,6 +58,9 @@ class ClientWrapper(object):
     self._sock = socket.socket()
     self._sock.connect(('localhost', 9010))
     self._client = OlaClient(self._sock)
+
+    self._events = []
+    heapq.heapify(self._events)
 
   def Stop(self):
     self._quit = True
@@ -44,6 +74,35 @@ class ClientWrapper(object):
   def Run(self):
     self._quit = False
     while not self._quit:
-      i, o, e = select.select([self._sock], [], [])
+
+      # default to 1s sleep
+      sleep_time = 1
+      now = datetime.datetime.now()
+      self.CheckTimeouts(now)
+      if len(self._events):
+        sleep_time = min(1.0, self._events[0].TimeLeft(now))
+
+      i, o, e = select.select([self._sock], [], [], sleep_time)
+      now = datetime.datetime.now()
+      self.CheckTimeouts(now)
       if self._sock in i:
         self._client.SocketReady()
+
+  def AddEvent(self, time_in_ms, callback):
+    """Schedule an event to run in the future.
+
+    Args:
+      time_in_ms: An interval in milliseconds when this should run.
+      callback: The function to run.
+    """
+    event = Event(time_in_ms, callback)
+    heapq.heappush(self._events, event)
+
+  def CheckTimeouts(self, now):
+    while len(self._events):
+      event = self._events[0]
+      if event.HasExpired(now):
+        event.Run()
+      else:
+        break
+      heapq.heappop(self._events)
