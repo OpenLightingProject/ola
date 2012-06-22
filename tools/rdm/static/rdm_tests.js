af9edb2c21ab02c789473706e9eae116a8ccc775
Ravindra Nath Kakarla <ravindhranath@gmail.com>
2012-06-22 18:39:03 +0530

    Updates device list

--- tools/rdm/static/rdm_tests.js
+++ tools/rdm/static/rdm_tests.js
@@ -20,6 +20,13 @@
 RDMTests = function() {
 };
 
+RDMTests.prototype.bind_events_to_doms = function() {
+  $('#universe_options').change(function() {
+    rdmtests = new RDMTests();
+    rdmtests.update_device_list();
+  });
+};
+
 RDMTests.prototype.query_server = function(request, params, callback) {
   $.ajax({
     url: request, 
@@ -38,16 +45,34 @@ RDMTests.prototype.update_universe_list = function() {
     if (data['status'] == true) {
       var universes = data.universes;
       var universe_options = $('#universe_options');
+      $('#universe_options').empty();
       $.each(universes, function(item) {
         universe_options.append($('<option />').val(universes[item]._id)
                                 .text(universes[item]._name));
+      });
+      rdmtests = new RDMTests();
+      rdmtests.update_device_list();
+    }
+  });
+};
+
+RDMTests.prototype.update_device_list = function() {
+  var universe_options = $('#universe_options');
+  var devices_list = $('#devices_list');
+  this.query_server('../GetDevices', { 'u': universe_options.val() }, function(data) {
+    if (data['status'] == true) {
+      devices_list.empty();
+      var uids = data.uids;
+      $.each(uids, function(item) {
+        devices_list.append($('<option />').val(uids[item])
+                                .text(uids[item]));
       });
     }
   });
 };
 
-
 $(document).ready(function() {
   rdmtests = new RDMTests();
+  rdmtests.bind_events_to_doms();
   rdmtests.update_universe_list();
 });
