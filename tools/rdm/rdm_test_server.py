cc875f55f9f518f69a3af1af7386615955d3b8b4
Simon Newton <nomis52@gmail.com>
2012-08-26 10:13:27 -0700

    * rename TestServer to rdm_test_server, install if requested

--- tools/rdm/rdm_test_server.py
+++ tools/rdm/rdm_test_server.py
@@ -13,7 +13,7 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #
-# TestServer.py
+# rdm_test_server.py
 # Copyright (C) 2012 Ravindra Nath Kakarla
 
 import cgi
