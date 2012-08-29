86d61b21139c653b19c865abb493b4a0fdc9bdd2
Simon Newton <nomis52@gmail.com>
2012-08-28 21:50:54 -0700

    * add the SLPServer and slp-client classes

---
+++ tools/slp/SLPServer.cpp
@@ -0,0 +1,459 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * SLPServer.cpp
+ * Copyright (C) 2012 Simon Newton
+ */
+
+#if HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <termios.h>
+
+#include <ola/BaseTypes.h>
+#include <ola/Callback.h>
+#include <ola/Logging.h>
+#include <ola/io/SelectServer.h>
+#include <ola/network/IPV4Address.h>
+#include <ola/network/InterfacePicker.h>
+#include <ola/network/NetworkUtils.h>
+#include <ola/network/Socket.h>
+#include <ola/network/TCPSocketFactory.h>
+
+#ifdef HAVE_LIBMICROHTTPD
+#include <ola/http/HTTPServer.h>
+#include <ola/http/OlaHTTPServer.h>
+#endif
+
+#include "tools/slp/SLPServer.h"
+#include "tools/slp/SLPServiceImpl.h"
+#include "common/rpc/StreamRpcChannel.h"
+
+#include <string>
+#include <vector>
+
+
+namespace ola {
+namespace slp {
+
+
+using ola::NewCallback;
+using ola::NewSingleCallback;
+using ola::network::HostToNetwork;
+using ola::network::IPV4Address;
+using ola::network::NetworkToHost;
+using ola::network::TCPAcceptingSocket;
+using ola::network::TCPSocket;
+using ola::network::UDPSocket;
+using ola::rpc::StreamRpcChannel;
+using std::auto_ptr;
+using std::string;
+using std::stringstream;
+using std::vector;
+
+
+const uint16_t SLPServer::DEFAULT_SLP_PORT = 427;
+const uint16_t SLPServer::DEFAULT_SLP_HTTP_PORT = 9012;
+const char SLPServer::K_SLP_PORT_VAR[] = "slp-port";
+
+/**
+ * Setup a new SLP server.
+ * @param socket the UDP Socket to use for SLP messages.
+ * @param options the SLP Server options.
+ */
+SLPServer::SLPServer(ola::network::UDPSocket *udp_socket,
+                     ola::network::TCPAcceptingSocket *tcp_socket,
+                     const SLPServerOptions &options,
+                     ola::ExportMap *export_map)
+    : m_iface_address(options.ip_address),
+      m_rpc_port(options.rpc_port),
+      m_ss(export_map),
+      m_tcp_socket_factory(NewCallback(this, &SLPServer::NewTCPConnection)),
+      m_tcp_accept_socket(&m_tcp_socket_factory),
+      m_udp_socket(udp_socket),
+      m_tcp_socket(tcp_socket),
+      m_export_map(export_map),
+      m_stdin_descriptor(STDIN_FILENO) {
+  m_multicast_address = IPV4Address(
+      HostToNetwork(239U << 24 |
+                    255U << 16 |
+                    255u << 8 |
+                    253));
+
+#ifdef HAVE_LIBMICROHTTPD
+  if (options.enable_http) {
+    ola::http::HTTPServer::HTTPServerOptions http_options;
+    http_options.port = options.http_port;
+
+    m_http_server.reset(
+        new ola::http::OlaHTTPServer(http_options, m_export_map));
+  }
+#endif
+}
+
+
+SLPServer::~SLPServer() {
+  // clean node list
+  NodeList::iterator node_iter = m_nodes.begin();
+  for (; node_iter != m_nodes.end(); ++node_iter) {
+    delete *node_iter;
+  }
+  m_nodes.clear();
+
+  m_udp_socket->Close();
+  m_tcp_accept_socket.Close();
+  tcsetattr(STDIN_FILENO, TCSANOW, &m_old_tc);
+}
+
+
+/**
+ * Init the server
+ */
+bool SLPServer::Init() {
+  OLA_INFO << "Interface address is " << m_iface_address;
+
+  // setup notifications for stdin & turn off buffering
+  m_stdin_descriptor.SetOnData(ola::NewCallback(this, &SLPServer::Input));
+  m_ss.AddReadDescriptor(&m_stdin_descriptor);
+  tcgetattr(STDIN_FILENO, &m_old_tc);
+  termios new_tc = m_old_tc;
+  new_tc.c_lflag &= static_cast<tcflag_t>(~ICANON & ~ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &new_tc);
+
+  // setup the accepting TCP socket
+  if (!m_tcp_accept_socket.Listen(m_iface_address, m_rpc_port)) {
+    return false;
+  }
+
+  m_ss.AddReadDescriptor(&m_tcp_accept_socket);
+
+  if (!m_udp_socket->SetMulticastInterface(m_iface_address)) {
+    m_tcp_accept_socket.Close();
+    return false;
+  }
+
+  // join the multicast group
+  if (!m_udp_socket->JoinMulticast(m_iface_address, m_multicast_address)) {
+    m_tcp_accept_socket.Close();
+    return false;
+  }
+
+  m_udp_socket->SetOnData(NewCallback(this, &SLPServer::UDPData));
+  m_ss.AddReadDescriptor(m_udp_socket);
+
+
+#ifdef HAVE_LIBMICROHTTPD
+  if (m_http_server.get())
+    m_http_server->Init();
+#endif
+
+  // setup the advertisment timeout, 30s
+  /*
+  m_ss.RegisterRepeatingTimeout(
+      TLP_ADVERTISMENT_PERIOD * 1000,
+      NewCallback(this, &SLPServer::SendPeriodicAdvert));
+  */
+  return true;
+}
+
+
+void SLPServer::Run() {
+#ifdef HAVE_LIBMICROHTTPD
+  if (m_http_server.get())
+    m_http_server->Start();
+#endif
+  m_ss.Run();
+}
+
+
+void SLPServer::Stop() {
+#ifdef HAVE_LIBMICROHTTPD
+  if (m_http_server.get())
+    m_http_server->Stop();
+#endif
+  m_ss.Terminate();
+}
+
+
+/**
+ * Called when a TCP socket is connected.
+ */
+void SLPServer::NewTCPConnection(TCPSocket *socket) {
+  IPV4Address peer_address;
+  uint16_t port;
+  socket->GetPeer(&peer_address, &port);
+  OLA_INFO << "New connection from " << peer_address << ":" << port;
+
+  /*
+  socket->SetOnData(
+      NewCallback(this, &SLPServer::ReceiveTCPData, socket));
+  socket->SetOnClose(
+    NewSingleCallback(this, &SLPServer::SocketClosed, socket));
+  m_ss.AddReadDescriptor(socket);
+  m_tcp_sockets.push_back(socket);
+  */
+  (void) socket;
+
+  /*
+  StreamRpcChannel *channel = new StreamRpcChannel(NULL, socket, NULL);
+  socket->SetOnClose(
+      NewSingleCallback(this, &SLPServer::TCPSocketClosed, socket));
+  OlaClientService_Stub *stub = new OlaClientService_Stub(channel);
+  Client *client = new Client(stub);
+  OlaClientService *service = m_service_factory->New(client, m_service_impl);
+  m_broker->AddClient(client);
+  channel->SetService(service);
+
+  map<int, OlaClientService*>::const_iterator iter;
+  iter = m_sd_to_service.find(socket->ReadDescriptor());
+
+  if (iter != m_sd_to_service.end())
+    OLA_INFO << "New socket but the client already exists!";
+
+  pair<int, OlaClientService*> pair(socket->ReadDescriptor(), service);
+  m_sd_to_service.insert(pair);
+
+  // This hands off ownership to the select server
+  m_ss->AddReadDescriptor(socket, true);
+  (*m_export_map->GetIntegerVar(K_CLIENT_VAR))++;
+  */
+}
+
+
+/**
+ * Called when a TCP socket is closed
+ */
+void SLPServer::TCPSocketClosed(TCPSocket *socket) {
+  OLA_INFO << "Socket closed";
+  (void) socket;
+}
+
+
+/**
+ * Called when there is data on the UDP socket
+ */
+void SLPServer::UDPData() {
+  ssize_t packet_size = 1500;
+  uint8_t packet[packet_size];
+  ola::network::IPV4Address source;
+  uint16_t port;
+
+  if (!m_udp_socket->RecvFrom(reinterpret_cast<uint8_t*>(&packet),
+                              &packet_size, source, port))
+    return;
+
+
+
+}
+
+
+/**
+ * Receive data on a TCP connection
+void SLPServer::ReceiveTCPData(TCPSocket *socket) {
+  uint8_t trash[512];
+  unsigned int data_length;
+  int ok = socket->Receive(trash, sizeof(trash), data_length);
+  if (ok == 0)  {
+    // OLA_INFO << "got " << data_length << " on a tcp connection";
+    // ola::FormatData(&cout, trash, data_length);
+    for (unsigned int i = 0; i < data_length; ++i) {
+      if (trash[i] == 'g') {
+        OLA_INFO << "Sending state";
+        SendState(socket);
+      }
+    }
+  }
+}
+
+
+ * Called when a socket is closed.
+void SLPServer::SocketClosed(TCPSocket *socket) {
+  auto_ptr<TCPSocket> socket_to_delete(socket);
+  OLA_INFO << "closing TCP socket";
+  m_ss.RemoveReadDescriptor(socket);
+
+  TCPSocketList::iterator iter = m_tcp_sockets.begin();
+  for (; iter != m_tcp_sockets.end(); ++iter) {
+    if (*iter == socket)
+      break;
+  }
+
+  if (iter == m_tcp_sockets.end()) {
+    OLA_FATAL << "Unable to locate socket for " << socket;
+    return;
+  }
+
+  m_tcp_sockets.erase(iter);
+}
+
+
+ * Send a join message to all connected clients
+void SLPServer::SendJoinUpdateToClients(const IPV4Address &address,
+                                        const UID &uid) {
+  stringstream str;
+  str << "Join: " << address << ", " << uid << endl;
+  SendStringToClients(str.str());
+}
+
+
+ * Send a part message to all connected clients
+void SLPServer::SendPartUpdateToClients(const IPV4Address &address,
+                                        const UID &uid) {
+  stringstream str;
+  str << "Part: " << address << ", " << uid << endl;
+  SendStringToClients(str.str());
+}
+
+
+ * Send a string to all clients
+void SLPServer::SendStringToClients(const string &output) {
+  TCPSocketList::iterator iter = m_tcp_sockets.begin();
+  for (; iter != m_tcp_sockets.end(); ++iter) {
+    (*iter)->Send(reinterpret_cast<const uint8_t*>(output.data()),
+                  output.size());
+  }
+  OLA_INFO << "Sent \"" << output.substr(0, output.size() - 1) << "\" to " <<
+    m_tcp_sockets.size() << " clients";
+}
+
+
+ * Send a full state message to a single client
+void SLPServer::SendState(TCPSocket *socket) {
+  NodeList::iterator iter = m_nodes.begin();
+  for ( ; iter != m_nodes.end(); ++iter) {
+    stringstream str;
+    str << "Active: " << (*iter)->ip << ", " << (*iter)->uid << endl;
+    string output = str.str();
+    socket->Send(reinterpret_cast<const uint8_t*>(output.data()),
+                 output.size());
+  }
+}
+
+*/
+
+/*
+ * Called when there is data on stdin.
+ */
+void SLPServer::Input() {
+  switch (getchar()) {
+    case 'q':
+      m_ss.Terminate();
+      break;
+    default:
+      break;
+  }
+}
+
+/*
+ * Send the periodic advertisment.
+bool SLPServer::SendPeriodicAdvert() {
+  OLA_INFO << "Sending advert";
+
+  ola::plugin::e131::OutgoingUDPTransport transport(&m_outgoing_udp_transport,
+                                                    m_multicast_address,
+                                                    TLP_PORT);
+
+  bool ok = m_root_sender.SendEmpty(TLP_REGISTRY_ADVERT_VECTOR, &transport);
+  if (!ok)
+    OLA_WARN << "Failed to send Advert";
+  return true;
+}
+
+
+ * Create a node entry
+void SLPServer::CreateNodeEntry(const IPV4Address &ip,
+                                const UID &uid,
+                                uint16_t lifetime) {
+  const TimeStamp *now = m_ss.WakeUpTime();
+
+  NodeList::iterator iter = m_nodes.begin();
+  for (; iter != m_nodes.end(); ++iter) {
+    if ((*iter)->ip == ip && (*iter)->uid == uid)
+      break;
+  }
+
+  TimeStamp expiry = *now + TimeInterval(lifetime, 0);
+  if (iter == m_nodes.end()) {
+    OLA_INFO << "creating " << ip << ", " << uid << ", " << lifetime;
+    NodeEntry *node = new NodeEntry(ip, uid, expiry);
+    m_nodes.push_back(node);
+    SendJoinUpdateToClients(ip, uid);
+  } else {
+    OLA_INFO << "updating " << ip << ", expires in " << lifetime << " seconds";
+    (*iter)->expiry = expiry;
+  }
+}
+
+
+ * Walk the list looking for stale entries
+bool SLPServer::LookForStaleEntries() {
+  OLA_INFO << "looking for stale entries";
+  const TimeStamp now = *(m_ss.WakeUpTime());
+
+  NodeList::iterator node_iter = m_nodes.begin();
+  while (node_iter != m_nodes.end()) {
+    if ((*node_iter)->expiry < now) {
+      OLA_INFO << "Node has expired " << (*node_iter)->ip << ", " <<
+        (*node_iter)->uid;
+      SendPartUpdateToClients((*node_iter)->ip, (*node_iter)->uid);
+      delete *node_iter;
+      node_iter = m_nodes.erase(node_iter);
+    } else {
+      node_iter++;
+    }
+  }
+  return true;
+}
+
+
+ * Called when we get a UDP message
+void SLPServer::UDPMessage(const IPV4Address &ip,
+                           uint32_t vector,
+                           const uint8_t *data,
+                           unsigned int length) {
+  OLA_INFO << "got udp message from " << ip << ", vector is " << vector;
+
+  if (vector == TLP_REGISTER_VECTOR) {
+    if (length != 8) {
+      OLA_WARN << "data length wasn't 8!";
+      return;
+    }
+
+    // extract the uid & lifetime
+    UID client_uid(data);
+    uint16_t lifetime;
+    memcpy(reinterpret_cast<void*>(&lifetime), data + 6, 2);
+    lifetime = NetworkToHost(lifetime);
+    OLA_INFO << "UID " << client_uid << ", lifetime " << lifetime;
+
+    CreateNodeEntry(ip, client_uid, lifetime);
+
+    // send an ack
+    ola::plugin::e131::OutgoingUDPTransport transport(
+        &m_outgoing_udp_transport,
+        ip,
+        TLP_PORT);
+
+    bool ok = m_root_sender.SendEmpty(TLP_REGISTER_ACK_VECTOR, &transport);
+    if (!ok)
+      OLA_WARN << "Failed to send ack";
+  } else {
+    OLA_INFO << "Got message with unknown vector " << vector;
+  }
+}
+ */
+}  // slp
+}  // ola
