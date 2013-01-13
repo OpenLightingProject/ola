aef0dd5af7b7bdc181d80b1d9f16668ab93e07c0
Simon Newton <nomis52@gmail.com>
2013-01-13 13:45:07 -0800

    * clean up the licence headers in the plugins/ directory

--- plugins/ftdidmx/FtdiWidget.h
+++ plugins/ftdidmx/FtdiWidget.h
@@ -1,31 +1,30 @@
 /*
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  Version 2 as published by the Free Software Foundation.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details. The license is
-  in the file "COPYING".
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-
-  This class is based on QLCFTDI class from
-
-  Q Light Controller
-  qlcftdi.h
-
-  Copyright (C) Heikki Junnila
-
-  Only standard CPP conversion was changed and function name changed
-  to follow OLA coding standards.
-
-  by
-  Rui Barreiros
-*/
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This class is based on QLCFTDI class from
+ *
+ * Q Light Controller
+ * qlcftdi.h
+ *
+ * Copyright (C) Heikki Junnila
+ *
+ * Only standard CPP conversion was changed and function name changed
+ * to follow OLA coding standards.
+ *
+ * by Rui Barreiros
+ */
 
 #ifndef PLUGINS_FTDIDMX_FTDIWIDGET_H_
 #define PLUGINS_FTDIDMX_FTDIWIDGET_H_
