4c8f16b32ac7c6ab102cdb46916039437c6bda4d
Simon Newton <nomis52@gmail.com>
2013-01-21 15:43:41 -0800

    * add all the LICENCE files and the script to check them

--- scripts/enforce_licence.py
+++ scripts/enforce_licence.py
@@ -18,55 +18,64 @@
 
 import difflib
 import getopt
+import glob
+import os
 import pprint
 import re
 import sys
 import textwrap
 
-LGPL = """/*
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
-"""
-
-GPL = """/*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Library General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
-"""
-
 def Usage(arg0):
   print textwrap.dedent("""\
-  Usage: %s [files]
+  Usage: %s
 
-  Check that the files start with the appropriate licence
+  Walk the directory tree from the current directory, and make sure all .cpp
+  and .h files have the appropriate Licence. The licence is determined from the
+  LICENCE file in each branch of the directory tree.
 
     --diff               Print the diffs.
     --fix                Fix the files.
-    --help               Display this message.
-    --license <GPL,LGPL> Type of license to check for.""" % arg0)
+    --help               Display this message.""" % arg0)
+
+def ParseArgs():
+  """Extract the options."""
+  try:
+    opts, args = getopt.getopt(sys.argv[1:], '',
+                               ['diff', 'fix', 'help'])
+  except getopt.GetoptError, err:
+    print str(err)
+    Usage(sys.argv[0])
+    sys.exit(2)
+
+  help = False
+  fix = False
+  diff = False
+  for o, a in opts:
+    if o in ('--diff'):
+      diff = True
+    elif o in ('--fix'):
+      fix = True
+    elif o in ('-h', '--help'):
+      Usage(sys.argv[0])
+      sys.exit()
+
+  if help:
+    Usage(sys.argv[0])
+    sys.exit(0)
+  return diff, fix
+
+def TransformLicence(licence):
+  """Wrap a licence in C++ style comments,"""
+  output = []
+  output.append('/*')
+  for l in licence:
+    l = l.strip()
+    if l:
+      output.append(' * %s' % l)
+    else:
+      output.append(' *')
+  output.append(' *')
+  return '\n'.join(output)
 
 def ReplaceHeader(file_name, new_header):
   f = open(file_name)
@@ -84,68 +93,71 @@ def ReplaceHeader(file_name, new_header):
 
   f = open(file_name, 'w')
   f.write(new_header)
+  f.write('\n')
   f.write(remainder)
   f.close()
 
-LICENCES = {
-  'GPL': GPL,
-  'LGPL': LGPL
-}
+def GetDirectoryLicences(root_dir):
+  """Walk the directory tree and determine the licence for each directory."""
+  LICENCE_FILE = 'LICENCE'
+  licences = {}
 
-def main():
-  try:
-    opts, args = getopt.getopt(sys.argv[1:], '',
-                               ['diff', 'fix', 'help', 'licence=', 'license='])
-  except getopt.GetoptError, err:
-    print str(err)
-    Usage(sys.argv[0])
-    sys.exit(2)
-
-  help = False
-  fix = False
-  diff = False
-  licence_name = 'GPL'
-  for o, a in opts:
-    if o in ('--diff'):
-      diff = True
-    elif o in ('--fix'):
-      fix = True
-    elif o in ('-h', '--help'):
-      Usage(sys.argv[0])
-      sys.exit()
-    elif o in ('--licence', '--license'):
-      licence_name = a
-
-  if help:
-    Usage(sys.argv[0])
-    sys.exit(0)
-
-  if not licence_name or licence_name.upper() not in LICENCES:
-    Usage(sys.argv[0])
-    sys.exit(0)
-
-  licence = LICENCES[licence_name.upper()]
-  header_size = len(licence)
-  d = difflib.Differ()
-
-  for file_name in args:
-    f = open(file_name)
-    header = f.read(header_size)
-    f.close()
-    if header == licence:
+  for dir_name, subdirs, files in os.walk(root_dir):
+    # skip the root_dir since the licence file is different there
+    if dir_name == root_dir:
       continue
 
-    if fix:
-      print 'Fixing %s' % file_name
-      ReplaceHeader(file_name, licence)
-    else:
-      print "File does not start with %s: %s" % (licence_name, file_name)
-      if diff:
-        result = list(d.compare(header.splitlines(1), licence.splitlines(1)))
-        pprint.pprint(result)
+    # don't descend into hidden dirs like .libs and .deps
+    subdirs[:] = [d for d in subdirs if not d.startswith('.')]
 
-    elif o in ('--licence', '--license'):
-      licence_name = a
+    if LICENCE_FILE in files:
+      f = open(os.path.join(dir_name, LICENCE_FILE))
+      lines = f.readlines()
+      f.close()
+      licences[dir_name] = TransformLicence(lines)
+      print 'Adding LICENCE for %s' % dir_name
+
+    # use this licence for all subdirs
+    licence = licences.get(dir_name)
+    if licence is not None:
+      for sub_dir in subdirs:
+        licences[os.path.join(dir_name, sub_dir)] = licence
+  return licences
+
+def CheckLicenceForDir(dir_name, licence, diff, fix):
+  """Check all files in a directory contain the correct licence."""
+  # glob doesn't support { } so we iterate instead
+  for match in ['*.h', '*.cpp']:
+    for file_name in glob.glob(os.path.join(dir_name, match)):
+      # skip the generated protobuf code
+      if '.pb.' in file_name:
+        continue
+      CheckLicenceForFile(file_name, licence, diff, fix)
+
+def CheckLicenceForFile(file_name, licence, diff, fix):
+  """Check a file contains the correct licence."""
+  f = open(file_name)
+  header_size = len(licence)
+  header = f.read(header_size)
+  f.close()
+  if header == licence:
+    return
+
+  if fix:
+    print 'Fixing %s' % file_name
+    ReplaceHeader(file_name, licence)
+  else:
+    print "File does not start with %s" % (file_name)
+    if diff:
+      d = difflib.Differ()
+      result = list(d.compare(header.splitlines(1), licence.splitlines(1)))
+      pprint.pprint(result)
+
+def main():
+  diff, fix = ParseArgs()
+  licences = GetDirectoryLicences(os.getcwd())
+  for dir_name, licence in licences.iteritems():
+    CheckLicenceForDir(dir_name, licence, diff=diff, fix=fix)
 
 if __name__ == '__main__':
   main()
