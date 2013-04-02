111d5ec225ed8067e196861c5b8ec94b6edd61c3
Peter Newman <peterjnewman@gmail.com>
2013-04-02 04:13:07 +0100

    Rename ola-uni-stats CPP file, to match binary name

--- examples/ola-uni-stats.cpp
+++ examples/ola-uni-stats.cpp
@@ -13,8 +13,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- *  ola-universe-stats.cpp
- *  Display some simple unvierse stats.
+ *  ola-uni-stats.cpp
+ *  Display some simple universe stats.
  *  Copyright (C) 2012 Simon Newton
  */
 
