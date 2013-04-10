eebe0b2759ed9c99fd4753dd4050143d267744bc
Simon Newton <nomis52@gmail.com>
2013-04-10 08:48:08 -0700

    * Patch from Andy.streubel to add comments to the python send dmx example

--- python/examples/ola_send_dmx.py
+++ python/examples/ola_send_dmx.py
@@ -28,11 +28,15 @@ def DmxSent(state):
 
 universe = 1
 data = array.array('B')
+#append first dmx-value
 data.append(10)
+#append second dmx-value
 data.append(50)
+#append third dmx-value
 data.append(255)
 
 wrapper = ClientWrapper()
 client = wrapper.Client()
+#send 1 dmx frame with values for channels 1-3
 client.SendDmx(universe, data, DmxSent)
 wrapper.Run()
