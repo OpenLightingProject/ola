13ce6a8ab6bb5c3bbbf5628ce6b07eeb117746cd
cpresser <c@rstenpresser.de>
2013-05-08 16:11:48 +0200

    changed coding-style

--- plugins/karate/Kl.h
+++ plugins/karate/Kl.h
@@ -19,9 +19,9 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301, USA
 #
-*/
-#ifndef _kl_h_
-#define _kl_h
+ */
+#ifndef PLUGINS_KARATE_KL_H_
+#define PLUGINS_KARATE_KL_H_
 
 #ifndef WIN32
 #include <stdint.h>
@@ -30,118 +30,129 @@
 #define uint8_t unsigned char
 #endif
 
-#define LIBKARATE_VERSION	"20130227"
-#define MAX_CHANNELS		512
-#define CMD_MAX_LENGTH		64
-#define MAX_ERROR_LENGTH	128
+#define LIBKARATE_VERSION  "20130227"
+#define MAX_CHANNELS       512
+#define CMD_MAX_LENGTH     64
+#define MAX_ERROR_LENGTH   128
 
 class KarateLight {
+  public:
+    explicit KarateLight(char * device);
+    ~KarateLight();
+    int Init();
 
-	public:
-		KarateLight(char * device);
-		~KarateLight();
-		int Init();
+    int SetColor(uint16_t chan, uint8_t val);
+    int SetColors(uint8_t* buffer, int length);
 
-		int SetColor(uint16_t chan, uint8_t val );
-		int SetColors(uint8_t* buffer, int length);
+    int Blank();
+    int UpdateColors();
 
-		int Blank ();
-		int UpdateColors();
+    unsigned int GetnChannels() {
+        return nChannels;
+    };
+    unsigned int GetFWVersion() {
+        return fw_version;
+    };
+    unsigned int GetHWVersion() {
+        return hw_version;
+    };
+    unsigned int GetDMXOffset() {
+        return dmx_offset;
+    };
+    int GetPWMValue();
+    int GetLDRValue();
 
-		unsigned int GetnChannels() { return nChannels;}; 
-		unsigned int GetFWVersion() { return fw_version;}; 
-		unsigned int GetHWVersion() { return hw_version;};
-		unsigned int GetDMXOffset() { return dmx_offset;};
-		int GetPWMValue();
-		int GetLDRValue();
+    int ReadEeprom(uint8_t addr);
+    int WriteEeprom(uint8_t addr, uint8_t data);
 
-		int ReadEeprom(uint8_t addr);
-		int WriteEeprom(uint8_t addr, uint8_t data);
+    int EnterBoot();
+    int SetPWMValue(unsigned int val);
 
-		int EnterBoot();
-		int SetPWMValue(unsigned int val);
+    char* GetLastError() {
+        return errortext;
+    }
+    bool IsActive() {
+        return active;
+    };
 
-		char* GetLastError() { return errortext;}
-		bool IsActive() { return active;};
+  private:
+    int CreateCommand(int cmd, uint8_t * data, int len);
+    int CalcChecksum(int len);
+    int ReadBack();
 
-	private:
-		int CreateCommand(int cmd, uint8_t * data, int len);
-		int CalcChecksum (int len);
-		int ReadBack();
+    char * devname;
+#ifdef WIN32
+    HANDLE fd;
+    DWORD bytesread;
+    DWORD byteswritten;
+#else
+    int fd;
+    int bytesread;
+    int byteswritten;
+#endif
 
-  		char * devname;
-		#ifdef WIN32
-		HANDLE fd;
-		DWORD bytesread;
-		DWORD byteswritten;
-		#else
-		int fd;
-		int bytesread;
-		int byteswritten;
-		#endif
+    unsigned int fw_version;
+    unsigned int hw_version;
+    unsigned int nChannels;
+    unsigned int dmx_offset;
 
-		unsigned int fw_version;
-		unsigned int hw_version;
-		unsigned int nChannels;
-		unsigned int dmx_offset;
+    uint8_t wr_buffer[CMD_MAX_LENGTH];
+    uint8_t rd_buffer[CMD_MAX_LENGTH];
 
-		uint8_t wr_buffer[CMD_MAX_LENGTH];
-		uint8_t rd_buffer[CMD_MAX_LENGTH];
+    unsigned int pwm_value;
+    unsigned int ldr_value;
 
-		unsigned int pwm_value;
-		unsigned int ldr_value;
+    uint8_t color_buffer[MAX_CHANNELS];
+    uint8_t color_buffer_old[MAX_CHANNELS];
+    uint8_t use_memcmp;
 
-		uint8_t color_buffer[MAX_CHANNELS];
-		uint8_t color_buffer_old[MAX_CHANNELS];
-		uint8_t use_memcmp;
-
-		bool active;
-		char errortext[MAX_ERROR_LENGTH];
+    bool active;
+    char errortext[MAX_ERROR_LENGTH];
 };
 
-#define KL_OK			0
-#define KL_ERROR		-1
-#define KL_CHECKSUMFAIL		-2
-#define KL_WRITEFAIL		-3
-#define KL_NOTACTIVE		-4
+#define KL_OK           0
+#define KL_ERROR        -1
+#define KL_CHECKSUMFAIL -2
+#define KL_WRITEFAIL    -3
+#define KL_NOTACTIVE    -4
 
 
 // address
-#define CMD_HD_SYNC		0x00
-#define CMD_HD_COMMAND		0x01
-#define CMD_HD_CHECK		0x02
-#define CMD_HD_LEN		0x03
-#define CMD_DATA_START		0x04
+#define CMD_HD_SYNC     0x00
+#define CMD_HD_COMMAND  0x01
+#define CMD_HD_CHECK    0x02
+#define CMD_HD_LEN      0x03
+#define CMD_DATA_START  0x04
 
 // sync words
-#define CMD_SYNC_SEND		0xAA
-#define CMD_SYNC_RECV		0x55
+#define CMD_SYNC_SEND       0xAA
+#define CMD_SYNC_RECV       0x55
 
 // status
-#define CMD_SYS_SYNC		0x00
-#define CMD_SYS_ACK		0x01		
-#define CMD_SYS_NACK		0x02
-#define CMD_SYS_NIMP		0xFF
-#define CMD_SYS_IR		0x10
-#define CMD_SYS_DATA		0x20
-#define CMD_SYS_NACK_LENGTH	0x03
-#define CMD_SYS_NACK_CHECK	0x04
+#define CMD_SYS_SYNC        0x00
+#define CMD_SYS_ACK         0x01
+#define CMD_SYS_NACK        0x02
+#define CMD_SYS_NIMP        0xFF
+#define CMD_SYS_IR          0x10
+#define CMD_SYS_DATA        0x20
+#define CMD_SYS_NACK_LENGTH 0x03
+#define CMD_SYS_NACK_CHECK  0x04
 
 // commands
-#define CMD_SYS_SYNC            0x00
-#define CMD_SYS_ACK             0x01
-#define CMD_SYS_NACK            0x02
-#define CMD_SYS_NIMP            0xFF
-#define CMD_SYS_IR		0x10
-#define CMD_SYS_DATA		0x20
-#define CMD_SYS_NACK_LENGTH	0x03
-#define CMD_SYS_NACK_CHECK	0x04
+#define CMD_SYS_SYNC        0x00
+#define CMD_SYS_ACK         0x01
+#define CMD_SYS_NACK        0x02
+#define CMD_SYS_NIMP        0xFF
+#define CMD_SYS_IR          0x10
+#define CMD_SYS_DATA        0x20
+#define CMD_SYS_NACK_LENGTH 0x03
+#define CMD_SYS_NACK_CHECK  0x04
 
-#define CMD_GET_VERSION		0x01
-#define CMD_GET_HARDWARE        0x02
+#define CMD_GET_VERSION     0x01
+#define CMD_GET_HARDWARE    0x02
 
-#define CMD_GET_TLC_PWM_VALUE	0x14
-#define CMD_SET_TLC_PWM_VALUE	0x15
+#define CMD_GET_TLC_PWM_VALUE   0x14
+#define CMD_SET_TLC_PWM_VALUE   0x15
 
 
 #define CMD_SET_DATA_00         0x20
@@ -164,19 +175,18 @@ class KarateLight {
 
 #define CMD_GET_N_CHANNELS      0x30
 
-#define CMD_READ_ADC0		0x40
+#define CMD_READ_ADC0       0x40
 
-#define CMD_READ_EEPROM		0x50
-#define CMD_WRITE_EEPROM	0x51
+#define CMD_READ_EEPROM     0x50
+#define CMD_WRITE_EEPROM    0x51
 
-#define CMD_BOOT_REQUEST	0x80
-#define CMD_BOOT_START		0x81
-
-
-
-#define HW_ID_KARATE            0x01
-#define HW_ID_USB2DMX           0x02
+#define CMD_BOOT_REQUEST    0x80
+#define CMD_BOOT_START      0x81
 
 
 
-#endif
+#define HW_ID_KARATE        0x01
+#define HW_ID_USB2DMX       0x02
+
+
+#endif  // PLUGINS_KARATE_KL_H_
