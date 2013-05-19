5f5835be3c940052173d5ad6eac6eb8f93c263fd
cpresser <c@rstenpresser.de>
2013-05-19 19:11:21 +0200

    filename changes

--- plugins/karate/KarateThread.cpp
+++ plugins/karate/KarateThread.cpp
@@ -35,7 +35,7 @@
 #include "ola/BaseTypes.h"
 #include "ola/Logging.h"
 #include "plugins/karate/KarateThread.h"
-#include "plugins/karate/Kl.h"
+#include "plugins/karate/KarateLight.h"
 
 namespace ola {
 namespace plugin {
