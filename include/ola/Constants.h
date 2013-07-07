b5276cea156e1f00dc0f837db8b1cc6477eb4469
Simon Newton <nomis52@gmail.com>
2013-07-07 09:48:43 -0700

    * Doc updates, deprecate BaseTypes.h in favor of Constants.h

--- include/ola/Constants.h
+++ include/ola/Constants.h
@@ -13,62 +13,63 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * BaseTypes.h
+ * Constants.h
  * Copyright (C) 2005-2009 Simon Newton
  */
 
 /**
- * @file BaseTypes.h
- * @deprecated Use Constants.h instead.
+ * @file Constants.h
+ * @brief Constants used throughout OLA.
  */
 
-#ifndef INCLUDE_OLA_BASETYPES_H_
-#define INCLUDE_OLA_BASETYPES_H_
+#ifndef INCLUDE_OLA_CONSTANTS_H_
+#define INCLUDE_OLA_CONSTANTS_H_
 
 #include <stdint.h>
-#include <ola/Constants.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+/**
+ * @brief The namespace containing all OLA symbols.
+ */
+namespace ola {
 
+/**
+ * @brief The number of slots in a DMX512 universe.
+ */
 enum {
-  DMX_UNIVERSE_SIZE = 512
+  DMX_UNIVERSE_SIZE = 512  ///< The number of slots in a DMX512 universe.
 };
 
 /**
  * @brief The minimum value a DMX512 slot can take.
  */
-static const uint8_t DMX_MIN_CHANNEL_VALUE = ola::DMX_MIN_SLOT_VALUE;
+static const uint8_t DMX_MIN_SLOT_VALUE = 0;
 
 /**
  * @brief The maximum value a DMX512 slot can take.
  */
-static const uint8_t DMX_MAX_CHANNEL_VALUE = ola::DMX_MAX_SLOT_VALUE;
+static const uint8_t DMX_MAX_SLOT_VALUE = 255;
 
 /**
  * @brief The start code for DMX512 data.
  * This is often referred to as NSC for "Null Start Code".
  */
-static const uint8_t DMX512_START_CODE = ola::DMX512_START_CODE;
+static const uint8_t DMX512_START_CODE = 0;
 
 /**
  * @brief The default port which olad listens on for incomming RPC connections.
  */
-static const int OLA_DEFAULT_PORT = ola::OLA_DEFAULT_PORT;
+static const int OLA_DEFAULT_PORT = 9010;
 
 /**
  * @brief The ESTA manufacturer code for the Open Lighting Project.
  */
-static const uint16_t OPEN_LIGHTING_ESTA_CODE = ola::OPEN_LIGHTING_ESTA_CODE;
+static const uint16_t OPEN_LIGHTING_ESTA_CODE = 0x7a70;
 
 /**
  * @brief The maximum universe number.
  */
-static const uint32_t MAX_UNIVERSE = ola::MAX_UNIVERSE;
+static const uint32_t MAX_UNIVERSE = 0xffffffff;
 
-#ifdef __cplusplus
-}
-#endif
+}  // ola
 
-#endif  // INCLUDE_OLA_BASETYPES_H_
+#endif  // INCLUDE_OLA_CONSTANTS_H_
