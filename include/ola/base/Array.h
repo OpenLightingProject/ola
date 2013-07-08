54ce708a0ee925f267876d3e1532eb04a71ce991
Sean Sill <sms3h2@gmail.com>
2013-07-08 19:54:11 -0400

    Documented Array.h

--- include/ola/base/Array.h
+++ include/ola/base/Array.h
@@ -17,21 +17,48 @@
  * A macro for determining array size.
  */
 
+/**
+ * @file Array.h
+ * @brief This file holds a macro for determing the array size of a staticly
+ * allocated array.
+ */
+
 #ifndef INCLUDE_OLA_BASE_ARRAY_H_
 #define INCLUDE_OLA_BASE_ARRAY_H_
 
 namespace ola {
 
-/*
+/**
+ * @brief Part of a helper to determine the size of a statically allocated
+ * array
+ * @tparam T is the type of your object.
+ * @tparam N is the size of your type T.
+ *
+ * @note
+ * Please see
  * http://src.chromium.org/svn/trunk/src/third_party/cld/base/macros.h
- * See that file for the gory details.
+ * for all the gory details
  */
 template <typename T, size_t N>
   char (&ArraySizeHelper(T (&array)[N]))[N];
 
+/**
+ * @brief Part of a helper to determine the size of a statically allocated
+ * const array
+ *
+ * @tparam T is the type of your object
+ * @tparam N is the size of your type T
+ */
 template <typename T, size_t N>
   char (&ArraySizeHelper(const T (&array)[N]))[N];
 
+
+/**
+ * @def arraysize(array)
+ * @brief Computes the size of the statically allocated array
+ * @param array is the array you wish to get the size of
+ * @return a size_t of the number of elements in the array
+ */
 #define arraysize(array) (sizeof(ArraySizeHelper(array)))
 
 }  // namespace ola
