7549881b916442650f2b2824ca61ff5a53dc2e4a
Simon Newton <nomis52@gmail.com>
2013-07-09 09:47:31 -0700

    * Document the STLUtils header

--- include/ola/stl/STLUtils.h
+++ include/ola/stl/STLUtils.h
@@ -18,6 +18,11 @@
  * Copyright (C) 2012 Simon Newton
  */
 
+/**
+ * @file STLUtils.h
+ * @brief Helper functions for STL types.
+ */
+
 #ifndef INCLUDE_OLA_STL_STLUTILS_H_
 #define INCLUDE_OLA_STL_STLUTILS_H_
 
@@ -34,37 +39,76 @@ using std::map;
 using std::set;
 using std::vector;
 
-// Helper functions for sequences.
-//------------------------------------------------------------------------------
-
 /**
- * Delete the elements of a Sequence.
+ * @brief Delete the elements of a Sequence.
+ * @param sequence the Sequence to delete the elements from.
+ * @tparam T A Sequence.
+ * @post The sequence is empty.
+ *
+ * All elements in the sequence will be deleted. The sequence will be cleared.
+ *
+ * @snippet
+ * ~~~~~~~~~~~~~~~~~~~~~
+ * class Foo() {
+ *  public:
+ *    Foo() {
+ *      // populate m_objects
+ *    }
+ *    ~Foo() {
+ *      STLDeleteElements(&m_objects);
+ *    }
+ *  private:
+ *    vector<Bar*> m_objects;
+ * };
+ * ~~~~~~~~~~~~~~~~~~~~~
  */
 template<typename T>
-void STLDeleteElements(T *values) {
-  typename T::iterator iter = values->begin();
-  for (; iter != values->end(); ++iter)
+void STLDeleteElements(T *sequence) {
+  typename T::iterator iter = sequence->begin();
+  for (; iter != sequence->end(); ++iter)
     delete *iter;
-  values->clear();
+  sequence->clear();
 }
 
-// Helper functions for associative containers.
-//------------------------------------------------------------------------------
-
 /**
- * Delete the values of an Associative Container.
+ * Delete all values in a pair associative container.
+ * @param container
+ * @tparam T A pair associative container.
+ * @post The container is empty.
+ *
+ * All elements in the container will be deleted. The container will be cleared.
+ *
+ * @snippet
+ * ~~~~~~~~~~~~~~~~~~~~~
+ * class Foo() {
+ *  public:
+ *    Foo() {
+ *      // populate m_objects
+ *    }
+ *    ~Foo() {
+ *      STLDeleteValues(&m_objects);
+ *    }
+ *  private:
+ *    map<int, Bar*> m_objects;
+ * };
+ * ~~~~~~~~~~~~~~~~~~~~~
+ * @sa STLDeleteElements.
  */
 template<typename T>
-void STLDeleteValues(T *values) {
-  typename T::iterator iter = values->begin();
-  for (; iter != values->end(); ++iter)
+void STLDeleteValues(T *container) {
+  typename T::iterator iter = container->begin();
+  for (; iter != container->end(); ++iter)
     delete iter->second;
-  values->clear();
+  container->clear();
 }
 
 
 /**
  * Returns true if the container contains the value.
+ * @tparam T1 A container.
+ * @tparam T2 The value to search for.
+ * @param container the container to search in.
+ * @param value the value to search for.
  */
 template<typename T1, typename T2>
 inline bool STLContains(const T1 &container, const T2 &value) {
@@ -72,7 +116,10 @@ inline bool STLContains(const T1 &container, const T2 &value) {
 }
 
 /**
- * Extract a vector of keys from an associative container.
+ * Extract the list of keys from a pair associative container.
+ * @tparam T1 A container.
+ * @param[in] container the container to extract the keys for.
+ * @param[out] keys the vector to populate with the keys.
  */
 template<typename T1>
 void STLKeys(const T1 &container, vector<typename T1::key_type> *keys) {
@@ -83,9 +130,13 @@ void STLKeys(const T1 &container, vector<typename T1::key_type> *keys) {
 }
 
 /**
- * Extract a vector of values from an associative container.
+ * @brief Extract a vector of values from a pair associative container.
+ * @tparam T1 A container.
+ * @tparam T2 The type of the values.
+ * @param[in] container the container to extract the values for.
+ * @param[out] values the vector to populate with the values.
  */
-template<typename T1>
+template<typename T1, typename T2>
 void STLValues(const T1 &container, vector<typename T1::mapped_type> *values) {
   values->reserve(values->size() + container.size());
   typename T1::const_iterator iter = container.begin();
@@ -94,8 +145,11 @@ void STLValues(const T1 &container, vector<typename T1::mapped_type> *values) {
 }
 
 /**
- * Lookup a value by key in a associative container and return a pointer to the
- * value. Returns NULL if the key doesn't exist.
+ * @brief Lookup a value by key in a associative container.
+ * @tparam T1 A container.
+ * @param container the container to search in.
+ * @param key the key to search for.
+ * @returns A pointer to the value, or NULL if the value isn't found.
  */
 template<typename T1>
 typename T1::mapped_type* STLFind(T1 *container,
@@ -109,8 +163,14 @@ typename T1::mapped_type* STLFind(T1 *container,
 }
 
 /**
- * Lookup a value by key in a associative container or pointers, or return a
- * NULL if the key doesn't exist.
+ * @brief Lookup a value by key in a associative container.
+ * @tparam T1 A container.
+ * @param container the container to search in.
+ * @param key the key to search for.
+ * @returns The value matching the key, or NULL if the value isn't found.
+ *
+ * This assumes that NULL can be co-erced to the mapped_type of the container.
+ * It's most sutiable for containers with pointers.
  */
 template<typename T1>
 typename T1::mapped_type STLFindOrNull(const T1 &container,
@@ -125,10 +185,19 @@ typename T1::mapped_type STLFindOrNull(const T1 &container,
 
 
 /**
- * Sets key : value, replacing any existing value. Note if value_type is a
- * pointer, you probably don't wait this since you'll leak memory if you
- * replace a value. Returns true if the value was replaced, false if it was
- * inserted.
+ * @brief Replace a value in a pair associative container, inserting the key,
+ * value if it doesn't already exist.
+ * @tparam T1 A container.
+ * @param container the container to replace the value in.
+ * @param key the key to insert / replace.
+ * @param value the value to insert / replace.
+ * @returns true if the value was replaced, false if the value was inserted.
+ *
+ * @note
+ * Note if the value type is a pointer, and the container has ownershop of the
+ * pointer, replacing a value will leak memory. Use STLReplaceAndDelete to
+ * avoid this.
+ * @sa STLReplaceAndDelete.
  */
 template<typename T1>
 bool STLReplace(T1 *container, const typename T1::key_type &key,
@@ -144,36 +213,52 @@ bool STLReplace(T1 *container, const typename T1::key_type &key,
 
 
 /**
- * Insert an key : value into a map where value is a pointer. This replaces the
- * previous value if it exists and deletes it.
+ * @brief Similar to STLReplace but this will delete the value if the
+ * replacement occurs.
+ * @tparam T1 A container.
+ * @param container the container to replace the value in.
+ * @param key the key to insert / replace.
+ * @param value the value to insert / replace.
+ * @returns true if the value was replaced, false if the value was inserted.
  */
 template<typename T1>
-void STLReplaceAndDelete(T1 *container, const typename T1::key_type &key,
+bool STLReplaceAndDelete(T1 *container, const typename T1::key_type &key,
                          const typename T1::mapped_type &value) {
   std::pair<typename T1::iterator, bool> p = container->insert(
       typename T1::value_type(key, value));
   if (!p.second) {
     delete p.first->second;
     p.first->second = value;
+    return true;
   }
+  return false;
 }
 
 
 /**
- * Insert a value into a container only if this value doesn't already exist.
- * Returns true if the key was inserted, false if the key already exists.
+ * @brief Insert a value into a container only if this value doesn't already
+ * exist.
+ * @tparam T1 A container.
+ * @param container the container to insert the value in.
+ * @param key_value the pair<key, value> to insert.
+ * @returns true if the key/value was inserted, false if the key already exists.
  */
 template<typename T1>
 bool STLInsertIfNotPresent(T1 *container,
-                           const typename T1::value_type &value) {
-  return container->insert(value).second;
+                           const typename T1::value_type &key_value) {
+  return container->insert(key_value).second;
 }
 
 
 /**
- * Insert an key : value into a map only if a value for this key doesn't
- * already exist.
- * Returns true if the key was inserted, false if the key already exists.
+ * @brief Insert a value into a container only if this value doesn't already
+ * exist.
+ * @tparam T1 A container.
+ * @param container the container to insert the value in.
+ * @param key the key to insert.
+ * @param value the value to insert.
+ * @returns true if the key/value was inserted, false if the key already exists.
+ * @sa STLInsertIfNotPresent.
  */
 template<typename T1>
 bool STLInsertIfNotPresent(T1 *container, const typename T1::key_type &key,
@@ -183,7 +268,14 @@ bool STLInsertIfNotPresent(T1 *container, const typename T1::key_type &key,
 
 
 /**
- * Insert an key : value into a map or die if the key already exists.
+ * @brief Insert an key : value into a pair associative container, or abort
+ * the program if the key already exists.
+ * @tparam T1 A container.
+ * @param container the container to insert the value in.
+ * @param key the key to insert.
+ * @param value the value to insert.
+ * @note
+ * This should only be used in unit-testing code.
  */
 template<typename T1>
 void STLInsertOrDie(T1 *container, const typename T1::key_type &key,
@@ -193,7 +285,10 @@ void STLInsertOrDie(T1 *container, const typename T1::key_type &key,
 
 
 /**
- * Remove a item from a container.
+ * @brief Remove a key / value from a container.
+ * @tparam T1 A container.
+ * @param container the container to remove the key from.
+ * @param key the key to remove.
  * @returns true if the item was removed, false otherwise.
  */
 template<typename T1>
@@ -202,9 +297,16 @@ bool STLRemove(T1 *container, const typename T1::key_type &key) {
 }
 
 /**
- * Lookup a value by key in a associative container. If the value exists, it's
- * removed from the container, the value placed in value and true is returned.
- * If the value doesn't exist this returns false.
+ * @brief Lookup and remove a key from a pair associative container.
+ * @tparam T1 A container.
+ * @param[in] container the container to remove the key from.
+ * @param[in] key the key to remove.
+ * @param[out] value A pointer which will be set to the removed value.
+ * @returns true if the item was found and removed, false otherwise.
+ *
+ * Lookup a value by key in a pair associative container. If the value exists,
+ * it's removed from the container, the value placed in value and true is
+ * returned.
  */
 template<typename T1>
 bool STLLookupAndRemove(T1 *container,
@@ -220,11 +322,13 @@ bool STLLookupAndRemove(T1 *container,
   }
 }
 
-
 /**
- * Lookup a value by key in a associative container. If the value exists, it's
- * deleted, removed from the container and true is returned.
- * If the value doesn't exist this returns false.
+ * @brief Remove a value from a pair associative container and delete it.
+ * @tparam T1 A container.
+ * @param[in] container the container to remove the key from.
+ * @param[in] key the key to remove.
+ * @returns true if the item was found and removed from the container, false
+ *   otherwise.
  */
 template<typename T1>
 bool STLRemoveAndDelete(T1 *container, const typename T1::key_type &key) {
@@ -238,11 +342,15 @@ bool STLRemoveAndDelete(T1 *container, const typename T1::key_type &key) {
   }
 }
 
-
 /**
- * Similar to STLLookupAndRemove but this operates on containers of pointers.
- * If the key exists, we remove it and return the value, if the key doesn't
- * exist we return NULL.
+ * @brief Remove a value from a pair associative container and return the value.
+ * @tparam T1 A container.
+ * @param[in] container the container to remove the key from.
+ * @param[in] key the key to remove.
+ * @returns The value matching the key, or NULL if the value isn't found.
+ *
+ * This assumes that NULL can be co-erced to the mapped_type of the container.
+ * It's most sutiable for containers with pointers.
  */
 template<typename T1>
 typename T1::mapped_type STLLookupAndRemovePtr(
