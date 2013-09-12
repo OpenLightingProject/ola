bdf252b8f6ed0b8d34ceb7790259a0dec7a07be0
Peter Newman <peterjnewman@gmail.com>
2013-09-12 15:47:53 +0100

    Add a test to check that seconday slots with an undefined type return a slot description

--- tools/rdm/TestDefinitions.py
+++ tools/rdm/TestDefinitions.py
@@ -587,7 +587,7 @@ class DetermineMaxPacketSize(ResponderTestFixture, DeviceInfoTest):
 
   def Test(self):
     if self.Property('supports_max_sized_pdl'):
-      self.SetNotRun(' Device supports full sized packet')
+      self.SetNotRun('Device supports full sized packet')
       return
 
     self._lower = 1
@@ -877,7 +877,7 @@ class FindSubDevices(ResponderTestFixture):
     # range or an ack
     if len(self._sub_device_addresses) == self._device_count:
       if self._device_count == 0:
-        self.SetNotRun(' No sub devices declared')
+        self.SetNotRun('No sub devices declared')
       self.SetProperty('sub_device_addresses', self._sub_device_addresses)
       self.SetProperty('sub_device_footprints', self._sub_device_footprints)
       self.Stop()
@@ -951,7 +951,7 @@ class GetParamDescription(ResponderTestFixture):
   def Test(self):
     self.params = self.Property('manufacturer_parameters')[:]
     if len(self.params) == 0:
-      self.SetNotRun(' No manufacturer params found')
+      self.SetNotRun('No manufacturer params found')
       return
     self._GetParam()
 
@@ -1728,7 +1728,7 @@ class GetPersonalityDescriptions(OptionalParameterTestFixture):
     self._current_index += 1
     if self._current_index > self._personality_count:
       if self._personality_count == 0:
-        self.SetNotRun(' No personalities declared')
+        self.SetNotRun('No personalities declared')
       self.SetProperty('personalities', self._personalities)
       self.Stop()
       return
@@ -2029,7 +2029,8 @@ class GetSlotInfo(OptionalParameterTestFixture):
   """Get SLOT_INFO."""
   CATEGORY = TestCategory.DMX_SETUP
   PID = 'SLOT_INFO'
-  PROVIDES = ['defined_slots', 'undefined_definition_slots']
+  PROVIDES = ['defined_slots', 'undefined_definition_slots',
+              'undefined_type_sec_slots']
 
   def Test(self):
     self.AddIfGetSupported(self.AckGetResult())
@@ -2039,11 +2040,13 @@ class GetSlotInfo(OptionalParameterTestFixture):
     if not response.WasAcked():
       self.SetProperty('defined_slots', set())
       self.SetProperty('undefined_definition_slots', [])
+      self.SetProperty('undefined_type_sec_slots', [])			
       return
 
     slots = [d['slot_offset'] for d in fields['slots']]
     self.SetProperty('defined_slots', set(slots))
     undefined_definition_slots = []
+    undefined_type_sec_slots = []
 
     for slot in fields['slots']:
       if slot['slot_type'] not in RDMConstants.SLOT_TYPE_TO_NAME:
@@ -2055,7 +2058,8 @@ class GetSlotInfo(OptionalParameterTestFixture):
         if slot['slot_label_id'] not in RDMConstants.SLOT_DEFINITION_TO_NAME:
           self.AddWarning('Unknown slot id %d for slot %d' %
                           (slot['slot_label_id'], slot['slot_offset']))
-        if slot['slot_label_id'] == 0xffff:
+        if (slot['slot_label_id'] ==
+            RDMConstants.SLOT_DEFINITIONS['SD_UNDEFINED']):
           undefined_definition_slots.append(slot['slot_offset'])
       else:
         # slot_label_id must reference a defined slot
@@ -2063,8 +2067,11 @@ class GetSlotInfo(OptionalParameterTestFixture):
           self.AddWarning(
               'Slot %d is of type secondary and references an unknown slot %d'
               % (slot['slot_offset'], slot['slot_label_id']))
+        if slot['slot_type'] == RDMConstants.SLOT_TYPES['ST_SEC_UNDEFINED']:
+          undefined_type_sec_slots.append(slot['slot_offset'])
 
     self.SetProperty('undefined_definition_slots', undefined_definition_slots)
+    self.SetProperty('undefined_type_sec_slots', undefined_type_sec_slots)
 
 
 class GetSlotInfoWithData(TestMixins.GetWithDataMixin,
@@ -2158,7 +2165,7 @@ class GetUndefinedSlotDefinitionDescriptions(OptionalParameterTestFixture):
   def Test(self):
     self.undef_slots = self.Property('undefined_definition_slots')[:]
     if len(self.undef_slots) == 0:
-      self.SetNotRun(' No undefined definition slots found')
+      self.SetNotRun('No undefined definition slots found')
       return
     self._GetSlotDescription()
 
@@ -2190,9 +2197,58 @@ class GetUndefinedSlotDefinitionDescriptions(OptionalParameterTestFixture):
             % (self.current_slot))
       return
 
-    if not fields['name'] :
+    if not fields['name']:
       self.AddWarning(
           'Slot description for slot %d with undefined definition was blank' %
+          (self.current_slot))
+      return
+
+
+class GetUndefinedSecondarySlotTypeDescriptions(OptionalParameterTestFixture):
+  """Get the slot description for all secondary slots with an undefined type."""
+  CATEGORY = TestCategory.DMX_SETUP
+  PID = 'SLOT_DESCRIPTION'
+  REQUIRES = ['undefined_type_sec_slots']
+
+  def Test(self):
+    self.undef_sec_slots = self.Property('undefined_type_sec_slots')[:]
+    if len(self.undef_sec_slots) == 0:
+      self.SetNotRun('No undefined type secondary slots found')
+      return
+    self._GetSlotDescription()
+
+  def _GetSlotDescription(self):
+    if len(self.undef_sec_slots) == 0:
+      self.Stop()
+      return
+
+    self.AddExpectedResults([
+      self.AckGetResult(action=self._GetSlotDescription),
+      self.NackGetResult(RDMNack.NR_UNKNOWN_PID,
+                         action=self._GetSlotDescription),
+      self.NackGetResult(RDMNack.NR_DATA_OUT_OF_RANGE,
+                         action=self._GetSlotDescription)
+    ])
+    self.current_slot = self.undef_sec_slots.pop()
+    self.SendGet(ROOT_DEVICE, self.pid, [self.current_slot])
+
+  def VerifyResult(self, response, fields):
+    if not response.WasAcked():
+      if response.nack_reason == RDMNack.NR_UNKNOWN_PID:
+        self.AddAdvisory(
+            '%s not supported for secondary slot %d with undefined type' %
+            (self.pid, self.current_slot))
+      if response.nack_reason == RDMNack.NR_DATA_OUT_OF_RANGE:
+        self.AddAdvisory(
+            'Slot description for secondary slot %d with undefined type was '
+            'missing'
+            % (self.current_slot))
+      return
+
+    if not fields['name']:
+      self.AddAdvisory(
+          'Slot description for secondary slot %d with undefined type was '
+          'blank' %
           (self.current_slot))
       return
 
@@ -2565,7 +2621,7 @@ class GetUndefinedSensorValues(OptionalParameterTestFixture):
       # loop and get all values
       self._GetSensorValue()
     else:
-      self.SetNotRun(' All sensors declared')
+      self.SetNotRun('All sensors declared')
       return
 
   def _GetSensorValue(self):
