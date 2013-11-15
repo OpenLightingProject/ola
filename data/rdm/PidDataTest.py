dccbe255b799d399578ef531c832346439709e30
Peter Newman <peterjnewman@gmail.com>
2013-11-15 23:20:55 +0000

    Make Python RDM loading test compatible with Python 2.6

--- data/rdm/PidDataTest.py
+++ data/rdm/PidDataTest.py
@@ -27,6 +27,13 @@ from ola import PidStore
 
 class PidDataTest(unittest.TestCase):
 
+  # Implement assertIsNotNone for Python runtimes < 2.7 or < 3.1
+  # Copied from:
+  # https://github.com/nvie/rq/commit/f5951900c8e79a116c59470b8f5b2f544000bf1f
+  if not hasattr(unittest.TestCase, 'assertIsNotNone'):
+    def assertIsNotNone(self, value, *args):
+      self.assertNotEqual(value, None, *args)
+
   def testLoad(self):
     store = PidStore.GetStore(os.environ['PIDDATA'])
     self.assertIsNotNone(store);
