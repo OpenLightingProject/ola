c4f306ededa4e9df13dcf6aa97328300795cde61
Peter Newman <peterjnewman@gmail.com>
2013-11-28 16:22:29 +0000

    Script to copy the doxygen doc

---
+++ doxygen/copy-doc.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ $# != 1 ]; then
+  echo "Usage: $0 <output_dir>"
+  exit 1;
+fi
+
+output_dir=$1
+if [ ! -d $output_dir ]; then
+  echo $output_dir is not a directory
+  exit 1;
+fi
+
+echo "Output dir: $output_dir";
+
+if [ ! -d ./html/ ]; then
+  echo ./html/ doesn\'t exist, make doxygen-doc failed somehow
+  exit 1;
+fi
+
+cp -a -v ./html/* $output_dir
+
+chmod -c -R a+r $output_dir
