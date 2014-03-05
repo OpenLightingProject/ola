ceae98b12d1e47fa2b09bdf64650c5b681b3fe0e
Simon Newton <nomis52@gmail.com>
2014-03-05 09:05:03 -0800

    Enable logging for all tests so we don't have to add it in every test.

--- common/testing/GenericTester.cpp
+++ common/testing/GenericTester.cpp
@@ -21,11 +21,15 @@
 #include <cppunit/CompilerOutputter.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
+#include "ola/Logging.h"
 
 int main(int argc, char* argv[]) {
   CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
   CppUnit::TextUi::TestRunner runner;
   runner.addTest(suite);
+  // Enable logging.
+  ola::InitLogging(ola::OLA_LOG_INFO, ola::OLA_LOG_STDERR);
+
   runner.setOutputter(
       new CppUnit::CompilerOutputter(&runner.result(), std::cerr));
   bool wasSucessful = runner.run();
