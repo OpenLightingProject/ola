553d5392ea81d6d6f23028f15bbe924c22799cca
Peter Newman <peterjnewman@gmail.com>
2014-06-12 00:42:35 +0100

    Stop ola_trigger segfaulting

--- tools/ola_trigger/ola-trigger.cpp
+++ tools/ola_trigger/ola-trigger.cpp
@@ -240,6 +240,14 @@ void NewDmx(unsigned int our_universe,
 
 
 /**
+ * The RegisterComplete Handler, this is called when registration finishes.
+ */
+void RegisterComplete(const string &error) {
+  OLA_INFO << "Register complete";
+  (void) error;
+}
+
+/**
  * Delete all the slot actions in the vector.
  */
 void FreeSlot(SlotList *slots) {
@@ -319,6 +327,7 @@ int main(int argc, char *argv[]) {
     exit(ola::EXIT_DATAERR);
   }
 
+  OLA_INFO << "Parsing file";
   yyparse();
 
   // if we got to this stage the config is ok, setup the client
@@ -342,7 +351,9 @@ int main(int argc, char *argv[]) {
     ola::OlaCallbackClient *client = wrapper.GetClient();
     client->SetDmxCallback(
         ola::NewCallback(&NewDmx, opts.universe, &trigger));
-    client->RegisterUniverse(opts.universe, ola::REGISTER, NULL);
+    client->RegisterUniverse(opts.universe,
+                             ola::REGISTER,
+                             ola::NewSingleCallback(&RegisterComplete));
 
     // start the client
     wrapper.GetSelectServer()->Run();
