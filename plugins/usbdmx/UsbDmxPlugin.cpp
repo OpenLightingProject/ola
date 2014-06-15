96171aa98095b153f758e58e5915269d2598023c
Peter Newman <peterjnewman@gmail.com>
2014-06-15 17:58:31 +0100

    Fix the build (cherry picked from commit f64e86aae11da04b66c81856acc34784bcc71d54)

--- plugins/usbdmx/UsbDmxPlugin.cpp
+++ plugins/usbdmx/UsbDmxPlugin.cpp
@@ -200,7 +200,7 @@ void UsbDmxPlugin::FindDevices() {
       device = new EuroliteProDevice(this, usb_device);
     } else {
       OLA_DEBUG << "Found an unknown device, skipping. VID: "
-                << device_descriptor.idVendor ", PID: "
+                << device_descriptor.idVendor << ", PID: "
                 << device_descriptor.idProduct;
     }
 
