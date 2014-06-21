3af90d1c42d55f0767dab7a045ad983c22a9b695
Simon Newton <nomis52@gmail.com>
2014-06-21 13:39:56 -0700

    Bump version

--- include/ola/base/Version.h
+++ include/ola/base/Version.h
@@ -29,7 +29,7 @@ namespace base {
 
 #define OLA_VERSION_MAJOR 0
 #define OLA_VERSION_MINOR 9
-#define OLA_VERSION_REVISION 0
+#define OLA_VERSION_REVISION 1
 
 /**
  * @class Version ola/base/Version.h
