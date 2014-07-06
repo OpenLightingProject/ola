e98742d1dbe6b29fffd7b434153d6f34d1826898
Simon Newton <nomis52@gmail.com>
2014-07-06 12:32:20 -0700

    Fix the $(EXEEXT) suffix

--- common/protocol/Makefile.mk
+++ common/protocol/Makefile.mk
@@ -18,8 +18,8 @@ common_libolacommon_la_LIBADD += $(libprotobuf_LIBS)
 # Find a better way to do this.
 # common_libolaproto_la_CXXFLAGS = $(COMMON_CXXFLAGS_ONLY_WARNINGS)
 
-common/protocol/Ola.pb.cc common/protocol/Ola.pb.h: common/protocol/Ola.proto$(EXEEXT)
+common/protocol/Ola.pb.cc common/protocol/Ola.pb.h: common/protocol/Ola.proto
 	$(PROTOC) --cpp_out common/protocol --proto_path $(srcdir)/common/protocol $(srcdir)/common/protocol/Ola.proto
 
-common/protocol/OlaService.pb.cpp common/protocol/OlaService.pb.h: common/protocol/Ola.proto protoc/ola_protoc
+common/protocol/OlaService.pb.cpp common/protocol/OlaService.pb.h: common/protocol/Ola.proto protoc/ola_protoc$(EXEEXT)
 	$(OLA_PROTOC)  --cppservice_out common/protocol --proto_path $(srcdir)/common/protocol $(srcdir)/common/protocol/Ola.proto
