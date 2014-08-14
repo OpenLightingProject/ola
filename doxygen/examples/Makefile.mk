126437d04f02592b31fb5b4c3e68d624820f687d
Simon Newton <nomis52@gmail.com>
2014-08-14 09:30:21 -0700

    Include the missing doxygen-examples in the build.

--- doxygen/examples/Makefile.mk
+++ doxygen/examples/Makefile.mk
@@ -1,6 +1,6 @@
 # The following should match what pkg-config --libs libola returns
-LDADD = ../../common/libolacommon.la \
-        ../../ola/libola.la
+DOXYGEN_EXAMPLES_LDADD = common/libolacommon.la \
+                         ola/libola.la
 
 if BUILD_EXAMPLES
 noinst_PROGRAMS += \
@@ -17,13 +17,24 @@ endif
 
 doxygen_examples_callback_client_transmit_SOURCES = \
     doxygen/examples/callback_client_transmit.cpp
+doxygen_examples_callback_client_transmit_LDADD = $(DOXYGEN_EXAMPLES_LDADD)
+
 doxygen_examples_flags_SOURCES = \
     doxygen/examples/flags.cpp
+doxygen_examples_flags_LDADD = $(DOXYGEN_EXAMPLES_LDADD)
+
 doxygen_examples_streaming_client_SOURCES = \
     doxygen/examples/streaming_client.cpp
+doxygen_examples_streaming_client_LDADD = $(DOXYGEN_EXAMPLES_LDADD)
+
 doxygen_examples_streaming_client_plugin_SOURCES = \
     doxygen/examples/streaming_client_plugin.cpp
+doxygen_examples_streaming_client_plugin_LDADD = $(DOXYGEN_EXAMPLES_LDADD)
+
 doxygen_examples_receiver_SOURCES = \
     doxygen/examples/receiver.cpp
+doxygen_examples_receiver_LDADD = $(DOXYGEN_EXAMPLES_LDADD)
+
 doxygen_examples_udp_server_SOURCES = \
     doxygen/examples/udp_server.cpp
+doxygen_examples_udp_server_LDADD = $(DOXYGEN_EXAMPLES_LDADD)
