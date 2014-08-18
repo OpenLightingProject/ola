749b34a14a9ecd3a1b7781b7e17015794f751e24
Simon Newton <nomis52@gmail.com>
2014-08-18 08:32:39 -0700

    Add a way to set a close callback for the ClientWrapper.
    Also document the ClientWrapper class.

--- ola/OlaClientWrapper.h
+++ ola/OlaClientWrapper.h
@@ -26,6 +26,7 @@
 #define OLA_OLACLIENTWRAPPER_H_
 
 #include <ola/AutoStart.h>
+#include <ola/Callback.h>
 #include <ola/OlaCallbackClient.h>
 #include <ola/client/OlaClient.h>
 #include <ola/io/SelectServer.h>
@@ -37,81 +38,123 @@
 namespace ola {
 namespace client {
 
-/*
- * The base class, not used directly.
+/**
+ * @brief The base Client Wrapper class.
+ *
+ * This encapsulates the calls required to setup a connection to olad.
  */
 class BaseClientWrapper {
  public:
-    BaseClientWrapper() {}
-    virtual ~BaseClientWrapper();
+  typedef Callback0<void> CloseCallback;
 
-    ola::io::SelectServer *GetSelectServer() { return &m_ss; }
+  BaseClientWrapper();
+  virtual ~BaseClientWrapper();
 
-    bool Setup();
-    bool Cleanup();
-    void SocketClosed();
+  /**
+   * @brief Set the callback to be run when the client socket is closed.
+   *
+   * The default action is to terminate the SelectServer. By setting a callback
+   * you can override thie behavior.
+   *
+   * @param callback, the Callback to run, ownership is transferred.
+   */
+  void SetCloseCallback(CloseCallback *callback);
+
+  /**
+   * @brief Get the SelectServer used by this client.
+   * @returns A pointer to a SelectServer, ownership isn't transferred.
+   */
+  ola::io::SelectServer *GetSelectServer() { return &m_ss; }
+
+  /**
+   * @brief Setup the client.
+   * @returns true on success, false on failure.
+   */
+  bool Setup();
+
+  /**
+   * @brief Reset the connection to the server.
+   * @returns true if the setup succeeded, false otherwise.
+   */
+  bool Cleanup();
+
+  /**
+   * @brief Called internally when the client socket is closed.
+   * @internal
+   */
+  void SocketClosed();
 
  protected:
-    std::auto_ptr<ola::network::TCPSocket> m_socket;
+  std::auto_ptr<ola::network::TCPSocket> m_socket;
 
  private:
-    ola::io::SelectServer m_ss;
+  ola::io::SelectServer m_ss;
+  std::auto_ptr<CloseCallback> m_close_callback;
 
-    virtual void CreateClient() = 0;
-    virtual bool StartupClient() = 0;
-    virtual void InitSocket() = 0;
+  virtual void CreateClient() = 0;
+  virtual bool StartupClient() = 0;
+  virtual void InitSocket() = 0;
 };
 
 
-/*
- * This wrapper uses the OlaClient class.
+/**
+ * @brief A templatized ClientWrapper.
  */
 template <typename ClientClass>
 class GenericClientWrapper: public BaseClientWrapper {
  public:
-    explicit GenericClientWrapper(bool auto_start = true):
-        BaseClientWrapper(),
-        m_auto_start(auto_start) {
-    }
-    ~GenericClientWrapper() {}
+  explicit GenericClientWrapper(bool auto_start = true):
+      BaseClientWrapper(),
+      m_auto_start(auto_start) {
+  }
+  ~GenericClientWrapper() {}
 
-    ClientClass *GetClient() const { return m_client.get(); }
+  /**
+   * @brief Return the underlying client object
+   */
+  ClientClass *GetClient() const { return m_client.get(); }
 
  private:
-    std::auto_ptr<ClientClass> m_client;
-    bool m_auto_start;
+  std::auto_ptr<ClientClass> m_client;
+  bool m_auto_start;
 
-    void CreateClient() {
-      if (!m_client.get()) {
-        m_client.reset(new ClientClass(m_socket.get()));
-      }
+  void CreateClient() {
+    if (!m_client.get()) {
+      m_client.reset(new ClientClass(m_socket.get()));
     }
+  }
 
-    bool StartupClient() {
-      bool ok = m_client->Setup();
-      m_client->SetCloseHandler(
-        ola::NewSingleCallback(static_cast<BaseClientWrapper*>(this),
-                               &BaseClientWrapper::SocketClosed));
-      return ok;
-    }
+  bool StartupClient() {
+    bool ok = m_client->Setup();
+    m_client->SetCloseHandler(
+      ola::NewSingleCallback(static_cast<BaseClientWrapper*>(this),
+                             &BaseClientWrapper::SocketClosed));
+    return ok;
+  }
 
-    void InitSocket() {
-      if (m_auto_start) {
-        m_socket.reset(ola::client::ConnectToServer(OLA_DEFAULT_PORT));
-      } else {
-        m_socket.reset(ola::network::TCPSocket::Connect(
-            ola::network::IPV4SocketAddress(
-              ola::network::IPV4Address::Loopback(),
-             OLA_DEFAULT_PORT)));
-      }
-      m_socket->SetNoDelay();
+  void InitSocket() {
+    if (m_auto_start) {
+      m_socket.reset(ola::client::ConnectToServer(OLA_DEFAULT_PORT));
+    } else {
+      m_socket.reset(ola::network::TCPSocket::Connect(
+          ola::network::IPV4SocketAddress(
+            ola::network::IPV4Address::Loopback(),
+           OLA_DEFAULT_PORT)));
     }
+    m_socket->SetNoDelay();
+  }
 };
 
+/**
+ * @brief A ClientWrapper that uses the OlaClient.
+ */
 typedef GenericClientWrapper<OlaClient> OlaClientWrapper;
 }  // namespace client
 
-// Legacy
+/**
+ * @brief A ClientWrapper that uses the OlaCallbackClient.
+ * @deprecated Use ola::client::OlaClientWrapper instead.
+ */
 typedef ola::client::GenericClientWrapper<OlaCallbackClient>
     OlaCallbackClientWrapper;
 
