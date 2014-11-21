5dfd7876d0fd89a53ecca65a05195a5ef4d04c3c
Peter Newman <peterjnewman@gmail.com>
2014-11-21 13:17:17 +0000

    Switch back to make + make check to see if that's what's stopping coveralls

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -34,7 +34,7 @@ elif [[ $TASK = 'doxygen' ]]; then
 else
   if [ "$CXX" = "g++-4.8" ]; then
     # Compile with coverage for coveralls
-    autoreconf -i && ./configure --enable-gcov && make distcheck
+    autoreconf -i && ./configure --enable-gcov && make && make check
   else
     # Otherwise compile and check as normal
     autoreconf -i && ./configure && make distcheck
