42bb492dde76040cd698aa5c0e9577eeea75900e
Simon Newton <nomis52@gmail.com>
2014-11-24 20:20:20 -0800

    Address Peter's comments.

--- plugins/usbdmx/LibUsbThread.cpp
+++ plugins/usbdmx/LibUsbThread.cpp
@@ -13,8 +13,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * AsyncPluginImpl.cpp
- * The asynchronous libusb implementation.
+ * LibUsbThread.cpp
+ * The thread for asynchronous libusb communication.
  * Copyright (C) 2014 Simon Newton
  */
 
