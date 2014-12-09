8b076505ccf9de937efac1dfa951a648b261c84a
Peter Newman <peterjnewman@gmail.com>
2014-12-09 19:03:48 +0000

    Remove an accidental extra test

--- common/utils/StringUtilsTest.cpp
+++ common/utils/StringUtilsTest.cpp
@@ -901,12 +901,6 @@ void StringUtilsTest::testStringJoin() {
   ints.push_back(3);
   OLA_ASSERT_EQ(string("1,2,3"), StringJoin(",", ints));
 
-  vector<uint8_t> bytes;
-  bytes.push_back(65);
-  bytes.push_back(66);
-  bytes.push_back(67);
-  OLA_ASSERT_EQ(string("65,66,67"), StringJoin(",", bytes));
-
   vector<char> chars;
   chars.push_back('a');
   chars.push_back('b');
