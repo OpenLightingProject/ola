cf88b09f80d17da7300e64e25ac4199793258bd5
Simon Newton <nomis52@gmail.com>
2015-01-20 07:21:07 -0800

    Avoid spurious warnings from the usb serial plugin.

--- include/ola/io/IOUtils.h
+++ include/ola/io/IOUtils.h
@@ -37,6 +37,17 @@ namespace io {
  * @returns true if the open succeeded, false otherwise.
  */
 bool Open(const std::string &path, int oflag, int *fd);
+
+/**
+ * @brief Wrapper around open().
+ * @param path the path to open
+ * @param oflag flags passed to open
+ * @param[out] fd a pointer to the fd which is returned.
+ * @returns true if the open succeeded, false otherwise.
+ *
+ * This is similar to Open(), but doesn't log a warning if open() fails.
+ */
+bool TryOpen(const std::string &path, int oflag, int *fd);
 }  // namespace io
 }  // namespace ola
 #endif  // INCLUDE_OLA_IO_IOUTILS_H_
