73d8ebdd205a1f9e039a2b88f1f634128cfb80c9
daveol <dave.olsthoorn@gmail.com>
2015-01-27 17:53:26 +0100

    cleanup and work web interface

--- olad/www/Makefile.mk
+++ olad/www/Makefile.mk
@@ -1,42 +1,42 @@
 wwwdir = $(www_datadir)
 
 dist_www_DATA = \
-    olad/www/index.html
-    olad/www/views/overview-universe.html
-    olad/www/views/add-universe.html
-    olad/www/views/info-plugins.html
-    olad/www/views/overview.html
-    olad/www/views/keypad-universe.html
-    olad/www/views/patch-universe.html
-    olad/www/views/settings-universe.html
-    olad/www/views/sliders-universe.html
-    olad/www/views/overview-universes.html
-    olad/www/views/rdm-universe.html
-    olad/www/js/app.js
-    olad/www/js/app.min.js
-    olad/www/libs/jquery/js/jquery.min.js
-    olad/www/libs/jquery/js/jquery.js
-    olad/www/libs/angular-route/js/angular-route.min.js
-    olad/www/libs/angular-route/js/angular-route.js
-    olad/www/libs/angular/js/angular.min.js
-    olad/www/libs/angular/js/angular.js
-    olad/www/libs/bootstrap/js/bootstrap.min.js
-    olad/www/libs/bootstrap/js/bootstrap.js
-    olad/www/libs/bootstrap/fonts/glyphicons-halflings-regular.woff
-    olad/www/libs/bootstrap/fonts/glyphicons-halflings-regular.svg
-    olad/www/libs/bootstrap/fonts/glyphicons-halflings-regular.ttf
-    olad/www/libs/bootstrap/fonts/glyphicons-halflings-regular.eot
-    olad/www/libs/bootstrap/fonts/glyphicons-halflings-regular.woff2
-    olad/www/libs/bootstrap/css/bootstrap.css.map
-    olad/www/libs/jquery/js/jquery.min.map
-    olad/www/libs/angular-route/js/angular-route.min.js.map
-    olad/www/js/app.min.js.map
-    olad/www/libs/angular/js/angular.min.js.map
-    olad/www/css/style.css
-    olad/www/css/style.min.css
-    olad/www/libs/bootstrap/css/bootstrap.min.css
-    olad/www/libs/bootstrap/css/bootstrap.css
-    olad/www/img/light_bulb.png
-    olad/www/img/logo.png
-    olad/www/img/light_bulb_off.png
+    olad/www/index.html \
+    olad/www/views/overview-universe.html \
+    olad/www/views/add-universe.html \
+    olad/www/views/info-plugins.html \
+    olad/www/views/overview.html \
+    olad/www/views/keypad-universe.html \
+    olad/www/views/patch-universe.html \
+    olad/www/views/settings-universe.html \
+    olad/www/views/sliders-universe.html \
+    olad/www/views/overview-universes.html \
+    olad/www/views/rdm-universe.html \
+    olad/www/js/app.js \
+    olad/www/js/app.min.js \
+    olad/www/libs/jquery/js/jquery.min.js \
+    olad/www/libs/jquery/js/jquery.js \
+    olad/www/libs/angular-route/js/angular-route.min.js \
+    olad/www/libs/angular-route/js/angular-route.js \
+    olad/www/libs/angular/js/angular.min.js \
+    olad/www/libs/angular/js/angular.js \
+    olad/www/libs/bootstrap/js/bootstrap.min.js \
+    olad/www/libs/bootstrap/js/bootstrap.js \
+    olad/www/libs/bootstrap/fonts/glyphicons-halflings-regular.woff \
+    olad/www/libs/bootstrap/fonts/glyphicons-halflings-regular.svg \
+    olad/www/libs/bootstrap/fonts/glyphicons-halflings-regular.ttf \
+    olad/www/libs/bootstrap/fonts/glyphicons-halflings-regular.eot \
+    olad/www/libs/bootstrap/fonts/glyphicons-halflings-regular.woff2 \
+    olad/www/libs/bootstrap/css/bootstrap.css.map \
+    olad/www/libs/jquery/js/jquery.min.map \
+    olad/www/libs/angular-route/js/angular-route.min.js.map \
+    olad/www/js/app.min.js.map \
+    olad/www/libs/angular/js/angular.min.js.map \
+    olad/www/css/style.css \
+    olad/www/css/style.min.css \
+    olad/www/libs/bootstrap/css/bootstrap.min.css \
+    olad/www/libs/bootstrap/css/bootstrap.css \
+    olad/www/img/light_bulb.png \
+    olad/www/img/logo.png \
+    olad/www/img/light_bulb_off.png \
     olad/www/img/logo-mini.png
