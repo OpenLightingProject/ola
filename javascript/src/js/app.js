43a12687be16396217bf66cb292338221b8a5e13
daveol <dave.olsthoorn@gmail.com>
2015-02-02 20:25:30 +0100

    add license banners and cleanup grunt

--- javascript/src/js/app.js
+++ javascript/src/js/app.js
@@ -1,5 +1,17 @@
 /**
-* Created by dave on 11-1-15.
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Library General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 angular
 .module('olaApp', ['ngRoute'])
@@ -155,20 +167,13 @@ angular
     $scope.Info = data;
   });
   $scope.Shutdown = function(){
-    $ola.action.Shutdown().then(function(data) {
-      console.log(data);
-    });
+    $ola.action.Shutdown().then(function(data) {});
   };
   $scope.goUniverse = function(id){
     $location.path('/universe/'+id);
   };
 }])
 .controller('addUniverseCtrl', ['$scope', '$ola', function ($scope, $ola) {
-  //post : /new_universe
-  //id:{{id}}
-  //name:{{name}}
-  //add_ports:{{ports}}
-
   $scope.Ports = {};
   $ola.get.Ports().then(function (data) {
     $scope.Ports = data;
