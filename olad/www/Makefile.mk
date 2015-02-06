11c929682566d9a4b1c1cb589954e1d1a02cd509
daveol <dave.olsthoorn@gmail.com>
2015-02-06 19:17:16 +0100

    fix indentation

--- olad/www/Makefile.mk
+++ olad/www/Makefile.mk
@@ -12,45 +12,45 @@ bootjsdir = $(www_datadir)/new/libs/bootstrap/js
 bootfontsdir = $(www_datadir)/new/libs/bootstrap/fonts
 
 dist_www_DATA = \
-				olad/www/back.png \
-				olad/www/blank.gif \
-				olad/www/button-bg.png \
-				olad/www/console_values.html \
-				olad/www/custombutton.css \
-				olad/www/discovery.png \
-				olad/www/editortoolbar.png \
-				olad/www/expander.png \
-				olad/www/forward.png \
-				olad/www/handle.vertical.png \
-				olad/www/hide_sections.png \
-				olad/www/incremental-discovery.png \
-				olad/www/landing.html \
-				olad/www/light_bulb.png \
-				olad/www/light_bulb_off.png \
-				olad/www/loader-mini.gif \
-				olad/www/loader.gif \
-				olad/www/logo-mini.png \
-				olad/www/logo.png \
-				olad/www/mobile.html \
-				olad/www/mobile.js \
-				olad/www/ola.html \
-				olad/www/ola.js \
-				olad/www/refresh.png \
-				olad/www/show_sections.png \
-				olad/www/tick.gif \
-				olad/www/toolbar-bg.png \
-				olad/www/toolbar.css \
-				olad/www/toolbar_sprites.png \
-				olad/www/vertical.gif \
-				olad/www/wand.png \
-				olad/www/warning.png
+    olad/www/back.png \
+    olad/www/blank.gif \
+    olad/www/button-bg.png \
+    olad/www/console_values.html \
+    olad/www/custombutton.css \
+    olad/www/discovery.png \
+    olad/www/editortoolbar.png \
+    olad/www/expander.png \
+    olad/www/forward.png \
+    olad/www/handle.vertical.png \
+    olad/www/hide_sections.png \
+    olad/www/incremental-discovery.png \
+    olad/www/landing.html \
+    olad/www/light_bulb.png \
+    olad/www/light_bulb_off.png \
+    olad/www/loader-mini.gif \
+    olad/www/loader.gif \
+    olad/www/logo-mini.png \
+    olad/www/logo.png \
+    olad/www/mobile.html \
+    olad/www/mobile.js \
+    olad/www/ola.html \
+    olad/www/ola.js \
+    olad/www/refresh.png \
+    olad/www/show_sections.png \
+    olad/www/tick.gif \
+    olad/www/toolbar-bg.png \
+    olad/www/toolbar.css \
+    olad/www/toolbar_sprites.png \
+    olad/www/vertical.gif \
+    olad/www/wand.png \
+    olad/www/warning.png
 dist_new_DATA = \
     olad/www/new/index.html
 dist_views_DATA = \
     olad/www/new/views/overview-universe.html \
     olad/www/new/views/add-universe.html \
     olad/www/new/views/info-plugins.html \
-				olad/www/new/views/info-plugin.html \
+    olad/www/new/views/info-plugin.html \
     olad/www/new/views/overview.html \
     olad/www/new/views/keypad-universe.html \
     olad/www/new/views/patch-universe.html \
