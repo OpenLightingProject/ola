3cbc3c4e70632769a02d7ac0334ca88067ac1d17
Simon Newton <nomis52@gmail.com>
2015-02-09 09:31:53 -0800

    Add the missing Makefile.mk

---
+++ tools/ja-rule/Makefile.mk
@@ -0,0 +1,14 @@
+if BUILD_JA_RULE
+bin_PROGRAMS += tools/ja-rule/ja-rule
+endif
+
+tools_ja_rule_ja_rule_SOURCES = \
+    tools/ja-rule/OpenLightingDevice.h \
+    tools/ja-rule/OpenLightingDevice.cpp \
+    tools/ja-rule/USBDeviceManager.cpp \
+    tools/ja-rule/USBDeviceManager.h \
+    tools/ja-rule/ja-rule.cpp
+tools_ja_rule_ja_rule_CXXFLAGS = $(COMMON_CXXFLAGS) $(libusb_CFLAGS)
+tools_ja_rule_ja_rule_LDADD = $(libusb_CFLAGS) \
+                              common/libolacommon.la \
+                              plugins/usbdmx/libolausbdmxwidget.la
