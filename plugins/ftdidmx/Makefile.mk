dbe49de493feec433d70175f661cbbdeb60e612a
Peter Newman <peterjnewman@gmail.com>
2015-02-23 23:45:45 +0000

    Tidy the end of ftd2xx potential support

--- plugins/ftdidmx/Makefile.mk
+++ plugins/ftdidmx/Makefile.mk
@@ -10,17 +10,8 @@ plugins_ftdidmx_libolaftdidmx_la_SOURCES = \
     plugins/ftdidmx/FtdiDmxPort.h \
     plugins/ftdidmx/FtdiDmxThread.cpp \
     plugins/ftdidmx/FtdiDmxThread.h \
-    plugins/ftdidmx/FtdiWidget-libftdi.cpp \
+    plugins/ftdidmx/FtdiWidget.cpp \
     plugins/ftdidmx/FtdiWidget.h
 plugins_ftdidmx_libolaftdidmx_la_LIBADD = $(libftdi_LIBS) \
                                           common/libolacommon.la
 endif
-
-# This isn't used yet.
-#if HAVE_LIBFTD2XX
-#  lib_LTLIBRARIES = plugins/ftdidmx/libolaftdidmx.la
-#  libolaftdidmx_la_SOURCES = FtdiDmxDevice.cpp FtdiDmxPlugin.cpp \
-#                             FtdiDmxThread.cpp FtdiWidget-ftd2xx.cpp
-#  libolaftdidmx_la_LIBADD = -lftd2xx \
-#                            ../../common/libolacommon.la
-#endif
