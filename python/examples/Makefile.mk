c7724444761cee7f77d33f691c532078e43a4a3a
Simon Marchi <simon.marchi@polymtl.ca>
2015-03-15 17:38:15 +0900

    Add ola_patch_unpatch.py to Makefile

--- python/examples/Makefile.mk
+++ python/examples/Makefile.mk
@@ -2,6 +2,7 @@
 dist_noinst_SCRIPTS += \
     python/examples/ola_artnet_params.py \
     python/examples/ola_devices.py \
+    python/examples/ola_patch_unpatch.py \
     python/examples/ola_plugin_info.py \
     python/examples/ola_rdm_discover.py \
     python/examples/ola_rdm_get.py \
