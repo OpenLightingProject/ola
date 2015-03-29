f6c7910553e2bbdc22b46a97c80ad1b1020c0c71
Simon Newton <nomis52@gmail.com>
2015-03-29 12:43:20 -0700

    Add a very basic Ja Rule controller that does discovery.

--- tools/ja-rule/Makefile.mk
+++ tools/ja-rule/Makefile.mk
@@ -1,7 +1,8 @@
 if BUILD_JA_RULE
 if !USING_WIN32
 if HAVE_LIBUSB_HOTPLUG_API
-bin_PROGRAMS += tools/ja-rule/ja-rule
+bin_PROGRAMS += tools/ja-rule/ja-rule \
+                tools/ja-rule/ja-rule-controller
 endif
 endif
 endif
@@ -16,3 +17,14 @@ tools_ja_rule_ja_rule_CXXFLAGS = $(COMMON_CXXFLAGS) $(libusb_CFLAGS)
 tools_ja_rule_ja_rule_LDADD = $(libusb_LIBS) \
                               common/libolacommon.la \
                               plugins/usbdmx/libolausbdmxwidget.la
+
+tools_ja_rule_ja_rule_controller_SOURCES = \
+    tools/ja-rule/OpenLightingDevice.h \
+    tools/ja-rule/OpenLightingDevice.cpp \
+    tools/ja-rule/USBDeviceManager.cpp \
+    tools/ja-rule/USBDeviceManager.h \
+    tools/ja-rule/ja-rule-controller.cpp
+tools_ja_rule_ja_rule_controller_CXXFLAGS = $(COMMON_CXXFLAGS) $(libusb_CFLAGS)
+tools_ja_rule_ja_rule_controller_LDADD = $(libusb_LIBS) \
+                                         common/libolacommon.la \
+                                         plugins/usbdmx/libolausbdmxwidget.la
