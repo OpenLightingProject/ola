9a508ec4f1d9e97ef4f4407bfd14bdb11f04da60
s-light <stefan@s-light.eu>
2015-05-16 10:18:07 +0200

    simplified CalculateAPA102LatchBytes
    as suggested by nomis52

--- plugins/spi/SPIOutput.cpp
+++ plugins/spi/SPIOutput.cpp
@@ -611,14 +611,9 @@ void SPIOutput::CombinedAPA102Control(const DmxBuffer &buffer) {
  * ( otherwise the return type must be changed to uint16_t)
  */
 uint8_t SPIOutput::CalculateAPA102LatchBytes(uint16_t pixel_count) {
-  // round up so that we get definitely more LatchBits as LEDs/2
-  // const uint8_t latch_bits = ceil(pixel_count / 2);
-  const uint8_t latch_bits = (pixel_count / 2)  + (pixel_count % 2 ? 1: 0);
-  OLA_DEBUG << "latch_bits: " << latch_bits;
-  // same for latch_bytes
-  // const uint8_t latch_bytes = ceil(latch_bits / 8);
-  const uint8_t latch_bytes = (latch_bits / 8) + (latch_bits % 8 ? 1: 0);
-  OLA_DEBUG << "latch_bytes: " << latch_bytes;
+  // round up so that we get definitely enough bits
+  const uint8_t latch_bits = (pixel_count + 1) / 2;
+  const uint8_t latch_bytes = (latch_bits + 7) / 8;
   return latch_bytes;
 }
 
