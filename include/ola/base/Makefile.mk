1e7b183d8a7a7c81d370f2e5b1f84006c548101d
Simon Newton <nomis52@gmail.com>
2015-06-16 10:25:33 -0700

    Don't include generated source files in the tarball.

--- include/ola/base/Makefile.mk
+++ include/ola/base/Makefile.mk
@@ -7,5 +7,7 @@ olabaseinclude_HEADERS = \
     include/ola/base/FlagsPrivate.h \
     include/ola/base/Init.h \
     include/ola/base/Macro.h \
-    include/ola/base/SysExits.h \
+    include/ola/base/SysExits.h
+
+nodist_olabaseinclude_HEADERS = \
     include/ola/base/Version.h
