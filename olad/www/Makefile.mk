0d0454bea83946b7c4551ef828ce347524ad5150
Peter Newman <peterjnewman@gmail.com>
2015-07-03 16:25:17 +0100

    Add the missing views HTML file

--- olad/www/Makefile.mk
+++ olad/www/Makefile.mk
@@ -47,17 +47,18 @@ dist_www_DATA = \
 dist_new_DATA = \
     olad/www/new/index.html
 dist_views_DATA = \
+    olad/www/new/views/overview.html \
     olad/www/new/views/plugins.html \
     olad/www/new/views/plugin-info.html \
-    olad/www/new/views/overview.html \
     olad/www/new/views/universes.html \
     olad/www/new/views/universe-add.html \
-    olad/www/new/views/universe-overview.html \
-    olad/www/new/views/universe-keypad.html \
-    olad/www/new/views/universe-patch.html \
-    olad/www/new/views/universe-settings.html \
     olad/www/new/views/universe-faders.html \
-    olad/www/new/views/universe-rdm.html
+    olad/www/new/views/universe-header.html \
+    olad/www/new/views/universe-keypad.html \
+    olad/www/new/views/universe-overview.html \
+    olad/www/new/views/universe-patch.html \
+    olad/www/new/views/universe-rdm.html \
+    olad/www/new/views/universe-settings.html
 dist_js_DATA = \
     olad/www/new/js/app.min.js
 dist_css_DATA = \
