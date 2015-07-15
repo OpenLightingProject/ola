57c1d366beeb835c323603fba99f223fa9c9cb3c
Peter Newman <peterjnewman@gmail.com>
2015-07-15 01:43:37 +0100

    Fix a header comment

--- olad/plugin_api/TestCommon.h
+++ olad/plugin_api/TestCommon.h
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * TestCommon2.h
+ * TestCommon.h
  * Test fixture for various olad classes
  * Copyright (C) 2005 Simon Newton
  */
