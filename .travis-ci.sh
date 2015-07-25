9a8623432081788ac909db7b0dc836ad846929f3
Dave Olsthoorn <dave.olsthoorn@gmail.com>
2015-07-25 20:30:42 +0200

    disable ja-rule and change to clang-3.7

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -40,7 +40,7 @@ elif [[ $TASK = 'check-licences' ]]; then
 elif [[ $TASK = 'doxygen' ]]; then
   # check doxygen only if it is the requested task
   autoreconf -i;
-  ./configure --enable-ja-rule;
+  ./configure --enable-rdm-tests # --enable-ja-rule;
   # the following is a bit of a hack to build the files normally built during
   # the build, so they are present for Doxygen to run against
   make builtfiles
@@ -57,7 +57,7 @@ elif [[ $TASK = 'doxygen' ]]; then
 elif [[ $TASK = 'coverage' ]]; then
   # Compile with coverage for coveralls
   autoreconf -i;
-  ./configure --enable-ja-rule --enable-gcov;
+  ./configure --enable-rdm-tests --enable-gcov # --enable-ja-rule;
   make;
   make check;
 elif [[ $TASK = 'coverity' ]]; then
@@ -76,7 +76,7 @@ elif [[ $TASK = 'jshint' ]]; then
 else
   # Otherwise compile and check as normal
   autoreconf -i;
-  ./configure --enable-rdm-tests --enable-ja-rule;
+  ./configure --enable-rdm-tests # --enable-ja-rule;
   make distcheck DISTCHECK_CONFIGURE_FLAGS='--enable-rdm-tests --enable-ja-rule';
   make dist;
   tarball=$(ls -Ut ola*.tar.gz | head -1)
