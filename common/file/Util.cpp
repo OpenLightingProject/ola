5a95138dc981ed203ff5d9579861df3f6c61e575
Simon Newton <nomis52@gmail.com>
2015-10-28 09:01:15 -0700

    Add UUCP locking.

--- common/file/Util.cpp
+++ common/file/Util.cpp
@@ -33,6 +33,7 @@
 #endif
 
 #include <algorithm>
+#include <fstream>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -57,6 +58,27 @@ string ConvertPathSeparators(const string &path) {
   std::replace(result.begin(), result.end(), '\\', PATH_SEPARATOR);
 #endif
   return result;
+}
+
+string JoinPaths(const string &first, const string &second) {
+  if (second.empty()) {
+    return first;
+  }
+
+  if (first.empty()) {
+    return second;
+  }
+
+  if (second[0] == PATH_SEPARATOR) {
+    return second;
+  }
+
+  string output(first);
+  if (output[output.size() - 1] != PATH_SEPARATOR) {
+    output.push_back(PATH_SEPARATOR);
+  }
+  output.append(second);
+  return output;
 }
 
 bool FindMatchingFiles(const string &directory,
