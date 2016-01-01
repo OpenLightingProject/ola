7596eb2afe98dc8e1ba46b246175076beb45b397
Peter Newman <peternewman@users.noreply.github.com>
2016-01-01 18:28:27 +0000

    Allow more code to be imported(cherry picked from commit c859d505415b22428b38a672453bd2cee6d5a960)

--- python/examples/ola_send_dmx.py
+++ python/examples/ola_send_dmx.py
@@ -29,17 +29,23 @@ wrapper = None
 def DmxSent(state):
   wrapper.Stop()
 
-universe = 1
-data = array.array('B')
-# append first dmx-value
-data.append(10)
-# append second dmx-value
-data.append(50)
-# append third dmx-value
-data.append(255)
 
-wrapper = ClientWrapper()
-client = wrapper.Client()
-# send 1 dmx frame with values for channels 1-3
-client.SendDmx(universe, data, DmxSent)
-wrapper.Run()
+def main():
+  universe = 1
+  data = array.array('B')
+  # append first dmx-value
+  data.append(10)
+  # append second dmx-value
+  data.append(50)
+  # append third dmx-value
+  data.append(255)
+
+  wrapper = ClientWrapper()
+  client = wrapper.Client()
+  # send 1 dmx frame with values for channels 1-3
+  client.SendDmx(universe, data, DmxSent)
+  wrapper.Run()
+
+
+if __name__ == '__main__':
+  main()
