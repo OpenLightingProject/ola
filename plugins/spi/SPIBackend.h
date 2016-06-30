96531ec7b07d50dcf5d0e812ff77a97e68e0619c
Derek Bever <derek.bever@gmail.com>
2016-06-30 16:56:00 +0000

    last changes for CHIP

--- plugins/spi/SPIBackend.h
+++ plugins/spi/SPIBackend.h
@@ -137,7 +137,7 @@ class HardwareBackend : public ola::thread::Thread,
 
   // GPIO members
   GPIOFds m_gpio_fds;
-  const std::vector<uint8_t> m_gpio_pins;
+  const std::vector<uint16_t> m_gpio_pins;
   std::vector<bool> m_gpio_pin_state;
 
   void SetupOutputs(Outputs *outputs);
