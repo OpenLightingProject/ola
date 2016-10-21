bedd2d1e05f93aba8ef10399a454f703dcea3679
Florian Edelmann <florian-edelmann@online.de>
2016-10-21 20:20:09 +0200

    Fixed small things * Typo form -> from * Typo obtail -> obtain * indention * added '>' as 'THRU' shortcut

--- javascript/new-src/src/directives/autofocus.js
+++ javascript/new-src/src/directives/autofocus.js
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Allows an element to obtail focus whenever a variable is set to true.
+ * Allows an element to obtain focus whenever a variable is set to true.
  * Copyright (C) 2016 Florian Edelmann
  */
 /*jshint browser: true, jquery: true*/
