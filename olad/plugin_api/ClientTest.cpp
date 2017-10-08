569f70ad05925d79c765d00fde7b8b24e1bde1e7
Peter Newman <peterjnewman@gmail.com>
2017-10-09 00:29:24 +0100

    Use the new OLA_ASSERT_DMX_EQUALS test

--- olad/plugin_api/ClientTest.cpp
+++ olad/plugin_api/ClientTest.cpp
@@ -118,14 +118,14 @@ void ClientTest::testGetSetDMX() {
   client.DMXReceived(TEST_UNIVERSE, source);
   const ola::DmxSource &source2 = client.SourceData(TEST_UNIVERSE);
   OLA_ASSERT(source2.IsSet());
-  OLA_ASSERT(source2.Data() == buffer);
+  OLA_ASSERT_DMX_EQUALS(source2.Data(), buffer);
   OLA_ASSERT_EQ(timestamp, source2.Timestamp());
   OLA_ASSERT_EQ((uint8_t) 100, source2.Priority());
 
   // check update works
   ola::DmxBuffer old_data(buffer);
   buffer.Set(TEST_DATA2);
-  OLA_ASSERT(source2.Data() == old_data);
+  OLA_ASSERT_DMX_EQUALS(source2.Data(), old_data);
   OLA_ASSERT_EQ(timestamp, source2.Timestamp());
   OLA_ASSERT_EQ((uint8_t) 100, source2.Priority());
 
@@ -133,12 +133,12 @@ void ClientTest::testGetSetDMX() {
   client.DMXReceived(TEST_UNIVERSE, source);
   const ola::DmxSource source3 = client.SourceData(TEST_UNIVERSE);
   OLA_ASSERT(source3.IsSet());
-  OLA_ASSERT(buffer == source3.Data());
+  OLA_ASSERT_DMX_EQUALS(buffer, source3.Data());
   OLA_ASSERT_EQ(timestamp, source3.Timestamp());
   OLA_ASSERT_EQ((uint8_t) 120, source3.Priority());
 
   // check fetching an unknown universe results in an empty buffer
   const ola::DmxSource source4 = client.SourceData(TEST_UNIVERSE2);
   OLA_ASSERT_FALSE(source4.IsSet());
-  OLA_ASSERT(empty == source4.Data());
+  OLA_ASSERT_DMX_EQUALS(empty, source4.Data());
 }
