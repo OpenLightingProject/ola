f58b0b0396839545d9d728cf0d666202e8348190
Peter Newman <peterjnewman@gmail.com>
2018-02-14 23:51:00 +0000

    Make the RDM test server a full URL so autolinking tools in terminals can use it

--- tools/rdm/rdm_test_server.py
+++ tools/rdm/rdm_test_server.py
@@ -1041,7 +1041,7 @@ def main():
   app = BuildApplication(ola_thread, test_thread)
 
   httpd = make_server('', settings['PORT'], app.HandleRequest)
-  logging.info('Running RDM Tests Server on %s:%s' %
+  logging.info('Running RDM Tests Server on http://%s:%s' %
                ('127.0.0.1', httpd.server_port))
 
   try:
