c6834e1261215ea6da845b773cca94c22685ca6f
Laurent Pierru <renzo@novatux.com>
2018-08-09 00:41:07 +0200

    Revert "fix the build on FreeBSD 10"
    This reverts commit a3ecdc0afca9188a8000919af20a65655e224fbe.

--- common/testing/Makefile.mk
+++ common/testing/Makefile.mk
@@ -6,6 +6,5 @@ noinst_LTLIBRARIES += common/testing/libolatesting.la \
 common_testing_libolatesting_la_SOURCES = \
     common/testing/MockUDPSocket.cpp \
     common/testing/TestUtils.cpp
-common_testing_libolatesting_la_CXXFLAGS = $(COMMON_TESTING_PROTOBUF_FLAGS)
 common_testing_libtestmain_la_SOURCES = common/testing/GenericTester.cpp
 endif
