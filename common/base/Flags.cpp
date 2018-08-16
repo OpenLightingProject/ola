10e1237f3a72c87892154a793f6e8e5161a6c2c9
Peter Newman <peterjnewman@gmail.com>
2018-08-16 13:26:17 +0100

    Prettify the man page version output

--- common/base/Flags.cpp
+++ common/base/Flags.cpp
@@ -358,8 +358,15 @@ void FlagRegistry::GenManPage() {
       str << " <" << flag->arg_type() << ">";
     }
     if (flag->short_opt()) {
-      short_flag_lines.push_back(
-          OptionPair(str.str(), iter->second->help()));
+      if (flag->name() == FLAGS_version.name()) {
+        std::ostringstream help_str;
+        help_str << "Print\n.B " << exe_name << "\nversion information.";
+        short_flag_lines.push_back(
+            OptionPair(str.str(), help_str.str()));
+      } else {
+        short_flag_lines.push_back(
+            OptionPair(str.str(), iter->second->help()));
+      }
     } else {
       long_flag_lines.push_back(
           OptionPair(str.str(), iter->second->help()));
