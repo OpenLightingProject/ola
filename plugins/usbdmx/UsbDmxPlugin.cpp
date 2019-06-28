fe65ce8fe1785a2b326eb9616e9693144e8a7926
André Offringa <offringa@gmail.com>
2019-06-28 21:35:39 +0200

    Fix an indentation as requested by peternewman

--- plugins/usbdmx/UsbDmxPlugin.cpp
+++ plugins/usbdmx/UsbDmxPlugin.cpp
@@ -110,7 +110,7 @@ bool UsbDmxPlugin::SetDefaultPreferences() {
 }
 
 void UsbDmxPlugin::ConflictsWith(
-  std::set<ola_plugin_id>* conflicting_plugins) const {
+    std::set<ola_plugin_id>* conflicting_plugins) const {
   if (EuroliteProFactory::IsEuroliteMk2Enabled(m_preferences)) {
     conflicting_plugins->insert(OLA_PLUGIN_USBPRO);
     conflicting_plugins->insert(OLA_PLUGIN_STAGEPROFI);
