684b95e38e6bf430b7c5f8a5a2719dab028a83a7
GertOdB <53154676+GertOdB@users.noreply.github.com>
2019-07-21 23:08:22 +0200

    Update HTTPServer.cpp
    This changes enable Cross-Origin Resource Sharing (CORS) as requested in here : https://github.com/OpenLightingProject/ola/issues/1547

--- common/http/HTTPServer.cpp
+++ common/http/HTTPServer.cpp
@@ -391,6 +391,7 @@ void HTTPResponse::SetHeader(const string &key, const string &value) {
  * @return true on success, false on error
  */
 int HTTPResponse::SendJson(const JsonValue &json) {
+  SetHeader("Access-Control-Allow-Origin", "*");
   const string output = JsonWriter::AsString(json);
   struct MHD_Response *response = HTTPServer::BuildResponse(
       static_cast<void*>(const_cast<char*>(output.data())),
@@ -412,6 +413,7 @@ int HTTPResponse::SendJson(const JsonValue &json) {
  * @return true on success, false on error
  */
 int HTTPResponse::Send() {
+  SetHeader("Access-Control-Allow-Origin", "*");
   HeadersMultiMap::const_iterator iter;
   struct MHD_Response *response = HTTPServer::BuildResponse(
       static_cast<void*>(const_cast<char*>(m_data.data())),
