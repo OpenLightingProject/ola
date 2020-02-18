bc2099bac694c2291f7dcb92cfb7025e4680218d
Peter Newman <peterjnewman@gmail.com>
2020-02-18 02:12:23 +0000

    Rename the target code to a more standardised name

--- tools/e133/Makefile.mk
+++ tools/e133/Makefile.mk
@@ -71,7 +71,7 @@ noinst_PROGRAMS += \
     tools/e133/e133_controller \
     tools/e133/e133_monitor \
     tools/e133/e133_receiver \
-    tools/e133/llrp_receive_test
+    tools/e133/llrp_target
 
 tools_e133_e133_receiver_SOURCES = tools/e133/e133-receiver.cpp
 tools_e133_e133_receiver_LDADD = common/libolacommon.la \
@@ -106,5 +106,5 @@ tools_e133_basic_device_LDADD = common/libolacommon.la \
                                 libs/acn/libolaacn.la \
                                 tools/e133/libolae133common.la
 
-tools_e133_llrp_receive_test_SOURCES = tools/e133/llrp-receive-test.cpp
-tools_e133_llrp_receive_test_LDADD = libs/acn/libolae131core.la
+tools_e133_llrp_target_SOURCES = tools/e133/llrp-target.cpp
+tools_e133_llrp_target_LDADD = libs/acn/libolae131core.la
