d7dbde4cc7956a211b8fee256ea5b400c80dd9b9
Peter Newman <peternewman@users.noreply.github.com>
2020-04-08 17:12:39 +0100

    Fix some more grammar

--- tools/rdm/ResponderTest.py
+++ tools/rdm/ResponderTest.py
@@ -78,7 +78,7 @@ class TestFixture(object):
 
   # TestFixture and its subclasses are equal/ordered based on comparing
   # class name.  Two TestFixtures are equal if they are the same class,
-  # and ordering is based comparing __class__.__name__
+  # and ordering is based on comparing __class__.__name__
   def __eq__(self, other):
     return other.__class__ is self.__class__
 
