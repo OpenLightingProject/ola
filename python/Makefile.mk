2c06f6283b1810829a6dcea469286221d23113d6
Bruce Lowekamp <bruce@lowekamp.net>
2020-08-08 17:25:21 -0400

    python: do not recompile files that are up to date to avoid race conditions

--- python/Makefile.mk
+++ python/Makefile.mk
@@ -5,7 +5,7 @@ python/PyCompileTest.sh: python/Makefile.mk
 	mkdir -p $(top_builddir)/python
 # 	restore this line when py3 compat is done for whole tree
 #	echo "$(PYTHON) -m compileall -f tools scripts python include data; exit \$$?" > $(top_builddir)/python/PyCompileTest.sh
-	echo "$(PYTHON) -m compileall -f python data; exit \$$?" > $(top_builddir)/python/PyCompileTest.sh
+	echo "$(PYTHON) -m compileall python data; exit \$$?" > $(top_builddir)/python/PyCompileTest.sh
 	chmod +x $(top_builddir)/python/PyCompileTest.sh
 
 if BUILD_PYTHON_LIBS
