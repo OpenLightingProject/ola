OpenLightingProject/ola

commit b82b97015abd96853cd6122f3f3c9d79bd0ed984

examples/ola_recorder: mitigate against drifting playback offset

examples/ShowPlayer.cpp
@@ -217,6 +217,8 @@ ShowLoader::State ShowPlayer::SeekTo(uint64_t seek_time) {
     }
   }
   m_playback_pos = playhead_time;
+  m_clock.CurrentTime(&m_start_ts);
+  m_start_playback_pos = m_playback_pos;
 
   // Send data in the state it would be in at the given time
   map<unsigned int, ShowEntry>::iterator entry_it;
@@ -270,8 +272,17 @@ void ShowPlayer::SendEntry(const ShowEntry &entry) {
   SendFrame(entry);
   m_playback_pos += entry.next_wait;
 
+  unsigned int timeout = entry.next_wait;
+  if (!m_simulate) {
+    uint64_t target_delta = m_playback_pos - m_start_playback_pos;
+    ola::TimeStamp now;
+    m_clock.CurrentTime(&now);
+    uint64_t delay = (target_delta - (now - m_start_ts).InMilliSeconds());
+    // Handle overflow
+    timeout = (delay >= (~((unsigned int)0))) ? 0 : delay;
+  }
   // Set when next to send data
-  RegisterNextTimeout(entry.next_wait);
+  RegisterNextTimeout(timeout);
 }
 
 

examples/ShowPlayer.h
@@ -18,6 +18,7 @@
  * Copyright (C) 2011 Simon Newton
  */
 
+#include <ola/Clock.h>
 #include <ola/DmxBuffer.h>
 #include <ola/client/ClientWrapper.h>
 
@@ -89,6 +90,9 @@ class ShowPlayer {
   uint64_t m_run_time;
   std::map<unsigned int, uint64_t> m_frame_count;
   bool m_simulate;
+  ola::Clock m_clock;
+  ola::TimeStamp m_start_ts;
+  uint64_t m_start_playback_pos;
 
   /** Used for tracking simulation progress */
   typedef enum {

commit c8938be6c2b3bf6df5cda5a7b6702b3aaef35cd5

examples/ola_recorder: fix overflow handling and warn on slow system.
Previous commit assumed that unsigned int is less wide than uint64_t when
handling unsigned overflow.

But since InMilliSeconds() returns a signed 64-bit value, discard all the
unsigned math and just do signed delta calculations (even though we are
losing one bit. Shouldn't really matter for the majority of cases,
unless someone's going to play for hundereds of years).

Also show a log message when the delay is negative, implying that the
system might be too slow to keep up with the current file. (We might
want to tune that to be < some negative number instead of just < 0
to avoid spamming too much?).

fixes: b82b97015abd96853cd6122f3f3c9d79bd0ed984

examples/ShowPlayer.cpp
@@ -274,12 +274,20 @@ void ShowPlayer::SendEntry(const ShowEntry &entry) {
 
   unsigned int timeout = entry.next_wait;
   if (!m_simulate) {
-    uint64_t target_delta = m_playback_pos - m_start_playback_pos;
+    // Using int64_t for target_delta here because
+    // we have to lose 1 bit anyway as InMilliSeconds() returns a signed
+    // 64-bit integer.
     ola::TimeStamp now;
     m_clock.CurrentTime(&now);
-    uint64_t delay = (target_delta - (now - m_start_ts).InMilliSeconds());
-    // Handle overflow
-    timeout = (delay >= (~((unsigned int)0))) ? 0 : delay;
+    int64_t target_delta = m_playback_pos - m_start_playback_pos;
+    int64_t current_delta = (now - m_start_ts).InMilliSeconds();
+    int64_t delay = target_delta - current_delta;
+    if (delay < 0) {
+      OLA_WARN << "Current frame was meant to be played in the past:"
+                  " System too slow?";
+      delay = 0;
+    }
+    timeout = delay;
   }
   // Set when next to send data
   RegisterNextTimeout(timeout);