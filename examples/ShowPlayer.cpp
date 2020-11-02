OpenLightingProject/ola

commit b82b97015abd96853cd6122f3f3c9d79bd0ed984

examples/ola_recorder: mitigate against drifting playback offset

examples/ShowPlayer.cpp
@@ -217,6 +217,8 @@ ShowLoader::State ShowPlayer::SeekTo(uint64_t seek_time) {
     }
   }
   m_playback_pos = playhead_time;
+  m_clock.CurrentTime(&m_start_ts);
+  m_start_playback_pos = m_playback_pos;
 
   // Send data in the state it would be in at the given time
   map<unsigned int, ShowEntry>::iterator entry_it;
@@ -270,8 +272,17 @@ void ShowPlayer::SendEntry(const ShowEntry &entry) {
   SendFrame(entry);
   m_playback_pos += entry.next_wait;
 
+  unsigned int timeout = entry.next_wait;
+  if (!m_simulate) {
+    uint64_t target_delta = m_playback_pos - m_start_playback_pos;
+    ola::TimeStamp now;
+    m_clock.CurrentTime(&now);
+    uint64_t delay = (target_delta - (now - m_start_ts).InMilliSeconds());
+    // Handle overflow
+    timeout = (delay >= (~((unsigned int)0))) ? 0 : delay;
+  }
   // Set when next to send data
-  RegisterNextTimeout(entry.next_wait);
+  RegisterNextTimeout(timeout);
 }
 
 

examples/ShowPlayer.h
@@ -18,6 +18,7 @@
  * Copyright (C) 2011 Simon Newton
  */
 
+#include <ola/Clock.h>
 #include <ola/DmxBuffer.h>
 #include <ola/client/ClientWrapper.h>
 
@@ -89,6 +90,9 @@ class ShowPlayer {
   uint64_t m_run_time;
   std::map<unsigned int, uint64_t> m_frame_count;
   bool m_simulate;
+  ola::Clock m_clock;
+  ola::TimeStamp m_start_ts;
+  uint64_t m_start_playback_pos;
 
   /** Used for tracking simulation progress */
   typedef enum {

commit 7ac4cc62f96cb7636fa9da2941e0079dde2e0311

examples/ola_recorder: fix start point tracking
We need to track the start point not at the playback time of the
next frame but the playback time of the current frame / partial frame.

fixes: b82b97015abd96853cd6122f3f3c9d79bd0ed984

examples/ShowPlayer.cpp
@@ -216,9 +216,10 @@ ShowLoader::State ShowPlayer::SeekTo(uint64_t seek_time) {
       break;
     }
   }
+  uint64_t timeout = playhead_time - seek_time;
   m_playback_pos = playhead_time;
   m_clock.CurrentTime(&m_start_ts);
-  m_start_playback_pos = m_playback_pos;
+  m_start_playback_pos = m_playback_pos - timeout;
 
   // Send data in the state it would be in at the given time
   map<unsigned int, ShowEntry>::iterator entry_it;
@@ -226,7 +227,7 @@ ShowLoader::State ShowPlayer::SeekTo(uint64_t seek_time) {
     SendFrame(entry_it->second);
   }
   // Adjust the timeout to handle landing in the middle of the entry's timeout
-  RegisterNextTimeout(playhead_time-seek_time);
+  RegisterNextTimeout(timeout);
 
   return ShowLoader::OK;
 }