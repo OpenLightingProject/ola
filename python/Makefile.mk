f636029cd5f8fb00e8a866f5288e20a1150b74bf
Bruce Lowekamp <bruce@lowekamp.net>
2020-11-15 14:59:50 -0500

    random build fixes

--- python/Makefile.mk
+++ python/Makefile.mk
@@ -3,7 +3,7 @@ include python/ola/Makefile.mk
 
 python/PyCompileTest.sh: python/Makefile.mk
 	mkdir -p $(top_builddir)/python
-	echo "$(PYTHON) -m compileall -f $(PYTHON_BUILD_DIRS); exit \$$?" > $(top_builddir)/python/PyCompileTest.sh
+	echo "$(PYTHON) -m compileall $(PYTHON_BUILD_DIRS); exit \$$?" > $(top_builddir)/python/PyCompileTest.sh
 	chmod +x $(top_builddir)/python/PyCompileTest.sh
 
 PYTHON_BUILD_DIRS += python
