b8e4500f2b56a9d92085935f97efa438f7ff5721
Peter Newman <peterjnewman@gmail.com>
2021-02-25 21:05:13 +0000

    Check for libsigrok and build the sigrok based sniffer

--- tools/logic/Makefile.mk
+++ tools/logic/Makefile.mk
@@ -2,11 +2,24 @@ if HAVE_SALEAE_LOGIC
 bin_PROGRAMS += tools/logic/logic_rdm_sniffer
 endif
 
+if HAVE_LIBSIGROK
+bin_PROGRAMS += tools/logic/sigrok_rdm_sniffer
+endif
+
 tools_logic_logic_rdm_sniffer_SOURCES = \
     tools/logic/DMXSignalProcessor.cpp \
     tools/logic/DMXSignalProcessor.h \
     tools/logic/logic-rdm-sniffer.cpp
 tools_logic_logic_rdm_sniffer_LDADD = common/libolacommon.la \
                                       $(libSaleaeDevice_LIBS)
+
+tools_logic_sigrok_rdm_sniffer_SOURCES = \
+    tools/logic/DMXSignalProcessor.cpp \
+    tools/logic/DMXSignalProcessor.h \
+    tools/logic/sigrok-rdm-sniffer.cpp
+tools_logic_sigrok_rdm_sniffer_CXXFLAGS = $(COMMON_CXXFLAGS) \
+                                          $(libsigrok_CFLAGS)
+tools_logic_sigrok_rdm_sniffer_LDADD = common/libolacommon.la \
+                                       $(libsigrok_LIBS)
 
 EXTRA_DIST += tools/logic/README.md
