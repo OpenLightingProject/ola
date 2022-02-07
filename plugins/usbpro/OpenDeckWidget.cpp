fb735da8a8488f3d3ad47900e66b45dce1949e21
paradajz <2544094+paradajz@users.noreply.github.com>
2022-02-07 19:48:56 +0100

    OpenDeckWidget: rework sending of data

--- plugins/usbpro/OpenDeckWidget.cpp
+++ plugins/usbpro/OpenDeckWidget.cpp
@@ -21,11 +21,15 @@
 #include <vector>
 
 #include "ola/Constants.h"
+#include "ola/Logging.h"
+#include "ola/util/Utils.h"
 #include "plugins/usbpro/OpenDeckWidget.h"
 
 namespace ola {
 namespace plugin {
 namespace usbpro {
+
+using ola::utils::SplitUInt16;
 
 /**
  * OpenDeckWidget Constructor
@@ -39,38 +43,48 @@ OpenDeckWidget::OpenDeckWidget(
 bool OpenDeckWidget::SendDMX(const DmxBuffer &data) {
   if (data != internal_buffer) {
     std::vector<uint8_t> send_buffer = {};
+    uint16_t changed_values = 0;
 
     for (size_t index = 0; index < data.Size(); index++) {
       if (data.Get(index) != internal_buffer.Get(index)) {
-        send_buffer.push_back((index+1) & 0xFF);
-        send_buffer.push_back((index+1) >> 8);
+        // In diff mode a packet is composed of 2 bytes of
+        // channel to update and 1 byte of actual channel
+        // value.
+        uint16_t channel = index+1;
+        uint8_t high;
+        uint8_t low;
+
+        SplitUInt16(channel, &high, &low);
+
+        send_buffer.push_back(low);
+        send_buffer.push_back(high);
         send_buffer.push_back(data.Get(index));
+
+        if (++changed_values >= MAX_DIFF_CHANNELS) {
+          // Just send the full frame in this case
+          struct {
+            uint8_t start_code;
+            uint8_t dmx[DMX_UNIVERSE_SIZE];
+          } full_frame;
+
+          full_frame.start_code = DMX512_START_CODE;
+          unsigned int length = DMX_UNIVERSE_SIZE;
+          data.Get(full_frame.dmx, &length);
+          internal_buffer = data;
+
+          return SendMessage(DMX_LABEL,
+                            reinterpret_cast<uint8_t*>(&full_frame),
+                            length + sizeof(full_frame.start_code));
+        }
       }
     }
 
     internal_buffer = data;
-
-    if (send_buffer.size() >= MAX_DIFF_CHANNELS) {
-      // Just send the full frame in this case
-      struct {
-        uint8_t start_code;
-        uint8_t dmx[DMX_UNIVERSE_SIZE];
-      } full_frame;
-
-      full_frame.start_code = DMX512_START_CODE;
-      unsigned int length = DMX_UNIVERSE_SIZE;
-      data.Get(full_frame.dmx, &length);
-
-      return SendMessage(DMX_LABEL,
-                         reinterpret_cast<uint8_t*>(&full_frame),
-                         length + sizeof(full_frame.start_code));
-    } else {
-      return SendMessage(DMX_SLOT_VALUE_DIFF_LABEL,
-                         &send_buffer[0],
-                         send_buffer.size());
-    }
+    return SendMessage(DMX_SLOT_VALUE_DIFF_LABEL,
+                        &send_buffer[0],
+                        send_buffer.size());
   } else {
-    // Nothing new to send
+    OLA_DEBUG << "Data unchanged - not sending data to device";
     return true;
   }
 }
