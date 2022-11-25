17818945cb0e3383d00f7f71019ece7a9b145d64
Peter Newman <peterjnewman@gmail.com>
2022-11-25 14:20:34 +0000

    Switch back to the same non-ascii as the responder test

--- python/ola/PidStoreTest.py
+++ python/ola/PidStoreTest.py
@@ -279,11 +279,11 @@ class PidStoreTest(unittest.TestCase):
       blob = pid._responses.get(PidStore.RDM_GET).Pack(args)[0]
 
     # test packing some non-ascii characters
-    args = ["\x0e\xc0"]
+    args = ["\x0d\xc0"]
     blob = pid._responses.get(PidStore.RDM_GET).Pack(args)[0]
     self.assertEqual(blob, binascii.unhexlify("0ec0"))
     decoded = pid.Unpack(blob, PidStore.RDM_GET)
-    self.assertEqual(decoded, {'languages': [{'language': '\x0e\xc0'}]})
+    self.assertEqual(decoded, {'languages': [{'language': '\x0d\xc0'}]})
 
     # valid empty string
     pid = store.GetName("STATUS_ID_DESCRIPTION")
