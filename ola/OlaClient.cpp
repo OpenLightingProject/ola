f007ee5d5e1d041a9f809341e6edfb8daca124b3
Perry Naseck <git@perrynaseck.com>
2023-02-17 16:15:56 -0500

    check-licence problem matcher works, revert force-fail files

--- ola/OlaClient.cpp
+++ ola/OlaClient.cpp
@@ -13,7 +13,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *
- * OlaClient.cppFAIL
+ * OlaClient.cpp
  * Implementation of OlaClient
  * Copyright (C) 2010 Simon Newton
  */
