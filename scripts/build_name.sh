1740d5a3aac7481e25de2eb172cfa4b0bf827d90
Perry Naseck <perry@hypersonic.cc>
2023-06-22 16:45:16 -0400

    add build name generation and bake into build

---
+++ scripts/build_name.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Library General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# build_name.sh
+# Copyright (C) 2023 Perry Naseck
+
+git describe --abbrev=7 --dirty --always --tags 2> /dev/null || echo 'unknown-out-of-tree'
