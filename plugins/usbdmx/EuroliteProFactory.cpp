92f1ba84b3009c0de7603a8c6030cccbcdea14be
André Offringa <offringa@gmail.com>
2024-01-14 16:41:12 +0100

    Textual review comment

--- plugins/usbdmx/EuroliteProFactory.cpp
+++ plugins/usbdmx/EuroliteProFactory.cpp
@@ -122,10 +122,10 @@ bool EuroliteProFactory::DeviceAdded(
 
     if (m_enable_eurolite_mk2 || serial_matches) {
       if (serial_matches) {
-        OLA_INFO << "Enabling new Eurolite USB-DMX512-PRO MK2 device "
+        OLA_INFO << "Found a probable new Eurolite USB-DMX512-PRO MK2 device "
                  << "with matching serial " << info.serial;
       } else {
-        OLA_INFO << "Found a possible new Eurolite USB-DMX512-PRO MK2 device "
+        OLA_INFO << "Found a probable new Eurolite USB-DMX512-PRO MK2 device "
                  << "with serial " << info.serial;
       }
       if (!m_adaptor->CheckManufacturer(EXPECTED_MANUFACTURER_MK2, info)) {
