2c2fdc792348c8a961cf92c024c1beff0406fa06
Peter Newman <peterjnewman@gmail.com>
2024-04-21 02:01:07 +0100

    Tidy a header file

--- libs/acn/E133StatusHelper.cpp
+++ libs/acn/E133StatusHelper.cpp
@@ -26,8 +26,11 @@
 namespace ola {
 namespace e133 {
 
-using ola::rdm::RDMStatusCode;
 using std::string;
+using ola::acn::RPTStatusVector;
+using ola::e133::E133StatusCode;
+using ola::e133::E133ConnectStatusCode;
+using ola::rdm::RDMStatusCode;
 
 /**
  * Verify that the int is a valid E1.33 Status Code.
